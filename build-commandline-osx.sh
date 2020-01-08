fd92a841b813e287a2358bf869c6d7255f2fc4fb
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-01-08 22:07:33 +0100

    removed unnecessary scripts

--- build-commandline-osx.sh
+++
@@ -1,8 +0,0 @@
-(
-	cd common
-	gradle build -x test
-)
-(
-	cd heap_jvm
-	gradle build -x test
-)
--- run-commandline-osx.sh
+++
@@ -1,9 +0,0 @@
-(
-	cd common
-	gradle build -x test
-)
-(
-	cd heap_jvm
-	gradle build -x test
-)
-java -cp ./heap_jvm/build/libs/heap_jvm.jar lupos/sparql1_1/test/TestKt
