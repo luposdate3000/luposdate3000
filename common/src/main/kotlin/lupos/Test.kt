9e97b0fbf9d1df21ab47c7501d767db4bbe0a9ad
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-01-14 13:49:13 +0100

    fix

--- common/src/main/kotlin/lupos/Test.kt
+++ common/src/main/kotlin/lupos/Test.kt
@@ -129,7 +129,7 @@ private fun readTurtleData(filename: String, consume_triple: (Long, Long, Long) 
     try {
         TurtleParserWithDictionary(consume_triple, ltit).turtleDoc()
     } catch (e: ParseError) {
-        println(e.message)
+	 e.kotlinStacktrace()
         println("Error in the following line:")
         println(e.lineNumber)
     }
