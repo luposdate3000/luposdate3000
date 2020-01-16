b9fe44ea81ab82f1a3d28b3020ca438eece14ec7
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-01-16 18:25:55 +0100

    start implementing message passing between two peers

--- common/src/main/kotlin/lupos/p2p/P2P.kt
+++ common/src/main/kotlin/lupos/p2p/P2P.kt
@@ -11,11 +11,54 @@ import com.soywiz.korio.concurrent.atomic.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.*
 import kotlin.coroutines.*
+import kotlinx.coroutines.channels.*
+
+val MESSAGE_HEADER_RESPONSE = 0
+val MESSAGE_HEADER_ECHO_STRING = 1
+
+abstract class Message() {
+    companion object {
+        var global_uuid = 0
+    }
+
+    var uuid = global_uuid++
+
+    suspend abstract fun write(client: AsyncClient): Message
+    suspend abstract fun read(client: AsyncClient): Message
+    suspend abstract fun readAndProcess(client: AsyncClient): Message
+}
+
+class MessageEchoString() : Message() {
+    var data = ""
+    suspend override fun read(client: AsyncClient): Message {
+        data = client.readStringz()
+        println("data = client.readStringz()")
+        return this
+    }
+
+    suspend override fun write(client: AsyncClient): Message {
+        client.write32LE(MESSAGE_HEADER_ECHO_STRING)
+        println("client.write32LE(MESSAGE_HEADER_ECHO_STRING)")
+        client.write32LE(uuid)
+        println("client.write32LE(uuid)")
+        client.writeStringz(data)
+        println("client.writeStringz(data)")
+        return this
+    }
+
+    suspend override fun readAndProcess(client: AsyncClient): Message {
+        read(client)
+        write(client)
+        return this
+    }
+}
 
 object P2P {
+
     var port: Int = 8080
     var bootstrap: String? = null
     private val clients = mutableMapOf<String, AsyncClient>()
+    private val channels = mutableMapOf<Int, Channel<Message>>()
     private var server: Any? = null
 
     suspend fun addClient(name: String, client: AsyncClient) {
@@ -23,10 +66,10 @@ object P2P {
         onConnect(name, client)
     }
 
-    suspend fun getClients(): List<AsyncClient> {
-        val res = mutableListOf<AsyncClient>()
+    suspend fun getClients(): List<String> {
+        val res = mutableListOf<String>()
         for ((k, v) in clients)
-            res.add(v)
+            res.add(k)
         return res
     }
 
@@ -35,6 +78,7 @@ object P2P {
             server = createTcpServer(port)
             (server!! as AsyncServer).listen { client ->
                 val name = client.readStringz()
+                println(" val name = client.readStringz()")
                 addClient(name, client)
             }
         }
@@ -49,26 +93,53 @@ object P2P {
                 }
                 val client = createTcpClient(host, port)
                 client.writeStringz((server!! as AsyncServer).host + ":" + (server!! as AsyncServer).port)
+                println("client.writeStringz(servername)")
                 addClient(bootstrap!!, client)
             }
         }
     }
 
     suspend fun onConnect(name: String, client: AsyncClient) {
-        println("onConnect $name")
+//        println("onConnect $name")
         var buffer = ByteArray(10)
         while (client.connected) {
-            println("server loop")
             var header = client.readS32LE()
+            println("var header = client.readS32LE()")
             onReceive(name, client, header)
         }
     }
 
     suspend fun onReceive(name: String, client: AsyncClient, header: Int) {
-        println("onReceive $name $header")
-//to p2p server calculations here
-        client.write32LE(header)
+        // println("onReceive $name $header")
+        if (header == MESSAGE_HEADER_RESPONSE) {
+            val header2 = client.readS32LE()
+            println("val header2 = client.readS32LE()")
+            val uuid = client.readS32LE()
+            println("val uuid = client.readS32LE()")
+            val message: Message = when (header2) {
+                MESSAGE_HEADER_ECHO_STRING -> MessageEchoString().read(client)
+                else -> throw Exception("unknown Message header $header")
+            }
+            channels[uuid]!!.send(message)
+        } else {
+            client.write32LE(MESSAGE_HEADER_RESPONSE)
+            println("client.write32LE(MESSAGE_HEADER_RESPONSE)")
+            val uuid = client.readS32LE()
+            println("val uuid = client.readS32LE()")
+            when (header) {
+                MESSAGE_HEADER_ECHO_STRING -> MessageEchoString().readAndProcess(client)
+            }
+        }
     }
+
+    suspend fun send_message(target: String, message: Message): Channel<Message> {
+        val client = clients[target]!!
+        val channel = Channel<Message>()
+        channels[message.uuid] = channel
+        message.write(client)
+        return channel
+    }
+
 }
 
 fun main(args: Array<String>) {
@@ -85,29 +156,21 @@ fun main(args: Array<String>) {
     }
     P2P.port = serverPort
     P2P.bootstrap = bootStrapServer
-    println("a")
     P2P.start()
-    println("b")
-    var result = async(Dispatchers.Default) {
-        println("c")
-        delay(1000)
-        println("d")
-        for (c in P2P.getClients()) {
-            println("e1")
-            c.write32LE(serverPort)
-            println("e4")
-            val rec = c.readS32LE()
-            println("e5")
-            println("recieve-main ${rec}")
-            println("f")
+    if (bootStrapServer != null) {
+        var result = async(Dispatchers.Default) {
+            delay(1000)
+            for (c in P2P.getClients()) {
+                val message = MessageEchoString()
+                message.data = "test"
+                val channel = P2P.send_message(c, message)
+                for (m in channel) {
+                    println("recieved-finally" + message.data)
+                }
+            }
+            delay(1000)
         }
-        println("g")
-        delay(1000)
-        println("h")
     }
-    println("i")
     while (true) {
     }
-    println("j")
-    println("stop")
 }
