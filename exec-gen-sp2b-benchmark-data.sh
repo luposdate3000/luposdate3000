5355412becebeff8622a84a179f21a1876219b11
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-01-21 14:51:47 +0100

    a

--- exec-gen-sp2b-benchmark-data.sh
+++ exec-gen-sp2b-benchmark-data.sh
@@ -6,13 +6,15 @@
 )
 cat /opt/sp2b/bin/sp2b.n3 | sed "s/\.$/ ./g" > ./common/src/main/resources/sp2b/sp2b.n3
 
-curl -X POST -d "@./common/src/main/resources/sp2b/sp2b.n3" -H "Content-Type: text/turtle" localhost:3030/sp2b/data
-
 pkill java
 
 /opt/apache-jena-fuseki-3.14.0/fuseki-server &
 
 sleep 3
+
+curl -X POST --data-urlencode "dbName=sp2b" --data-urlencode "dbType=mem" -H  "Content-Type: application/x-www-form-urlencoded; charset=UTF-8" localhost:3030/$/datasets
+
+curl -X POST -d "@./common/src/main/resources/sp2b/sp2b.n3" -H "Content-Type: text/turtle" localhost:3030/sp2b/data
 
 (
 	cd common/src/main/resources/sp2b
