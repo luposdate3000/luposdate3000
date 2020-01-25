b3bc1c938f336e3393167ebac8f785e62553f73a
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-01-25 09:42:51 +0100

    comiling in linux partially

--- build.gradle.native.kts
+++ build.gradle.native.kts
@@ -48,7 +48,7 @@ kotlin {
 //			kotlin.srcDir("src/linuxX64Main/kotlin")
 			kotlin.srcDir("src/commonMain/kotlin")
 //			kotlin.srcDir("src/commonBufferMainmemoryMain/kotlin")
-//			kotlin.srcDir("src/commonResultDictionaryNoneMain/kotlin")
+			kotlin.srcDir("src/commonResultDictionaryNoneMain/kotlin")
 //			kotlin.srcDir("src/commonTripleStoreHashMapMain/kotlin")
 			dependencies {
 				implementation(kotlin("stdlib", KotlinCompilerVersion.VERSION))
