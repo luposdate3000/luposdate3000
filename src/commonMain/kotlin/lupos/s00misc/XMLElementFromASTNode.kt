3f80a95e3fd4e93d3e24b217f8c3657f5bb3e852
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-01-29 11:17:50 +0100

    format

--- src/commonMain/kotlin/lupos/s00misc/XMLElementFromASTNode.kt
+++ src/commonMain/kotlin/lupos/s00misc/XMLElementFromASTNode.kt
@@ -1,68 +1,70 @@
 package lupos.s00misc
+
 import lupos.s02buildSyntaxTree.sparql1_1.*
-fun XMLElement.Companion.parseFromASTNode(node:ASTNode): XMLElement {
-when {
-                node is ASTVar -> return XMLElement("ASTVar").addAttribute("name", node.name)
-                node is ASTAddition -> return XMLElement("ASTAddition").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTAggregation -> {
-                    val res = XMLElement("ASTAggregation").addAttribute("type", "" + node.type).addAttribute("distinct", "" + node.distinct)
-                    node.children.forEach() {
-                        res.addContent(parseFromASTNode(it))
-                    }
-                    return res
-                }
-                node is ASTAnd -> {
-                    val res = XMLElement("ASTAnd")
-                    node.children.forEach() {
-                        res.addContent(parseFromASTNode(it))
-                    }
-                    return res
-                }
-                node is ASTBuiltInCall -> {
-                    val res = XMLElement("ASTBuiltInCall").addAttribute("function", "" + node.function)
-                    node.children.forEach() {
-                        res.addContent(parseFromASTNode(it))
-                    }
-                    return res
-                }
-                node is ASTDivision -> return XMLElement("ASTDivision").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTEQ -> return XMLElement("ASTEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTFunctionCall -> {
-                    val res = XMLElement("ASTFunctionCall").addAttribute("iri", node.iri).addAttribute("distinct", "" + node.distinct)
-                    return res
-                }
-                node is ASTGEQ -> return XMLElement("ASTGEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTGT -> return XMLElement("ASTGT").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTInteger -> return XMLElement("ASTInteger").addAttribute("value", "" + node.value)
-                node is ASTIn -> return XMLElement("ASTIn").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTIri -> return XMLElement("ASTIri").addAttribute("iri", node.iri)
-                node is ASTLEQ -> return XMLElement("ASTLEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTLT -> return XMLElement("ASTLT").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTMultiplication -> return XMLElement("ASTMultiplication").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTNotIn -> return XMLElement("ASTNotIn").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-                node is ASTNot -> return XMLElement("ASTNot").addContent(parseFromASTNode(node.children[0]))
-                node is ASTOr -> {
-                    val res = XMLElement("ASTOr")
-                    node.children.forEach() {
-                        res.addContent(parseFromASTNode(it))
-                    }
-                    return res
-                }
-node is ASTBooleanLiteral->return XMLElement("ASTBooleanLiteral").addAttribute("value", "" + node.value)
-node is ASTDecimal->return XMLElement("ASTDecimal").addAttribute("value", "" + node.image)
-node is ASTNEQ->return XMLElement("ASTNEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
-node is ASTSet-> {
-                    val res = XMLElement("ASTSet")
-                    node.children.forEach() {
-                        res.addContent(parseFromASTNode(it))
-                    }
-                    return res
-                }
-node is ASTSimpleLiteral->return XMLElement("ASTSimpleLiteral").addAttribute("content",node.content).addAttribute("delimiter",node.delimiter)
-node is ASTTriple->return XMLElement("ASTTriple").addContent(XMLElement("childS").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childP").addContent(parseFromASTNode(node.children[1]))).addContent(XMLElement("childO").addContent(parseFromASTNode(node.children[2])))
-node is ASTTypedLiteral->return XMLElement("ASTTypedLiteral").addAttribute("content",node.content).addAttribute("delimiter",node.delimiter).addAttribute("type_iri",node.type_iri)
-node is ASTFilter ->return XMLElement("ASTFilter").addContent(parseFromASTNode(node.children[0]))
-node is ASTLanguageTaggedLiteral ->return XMLElement("ASTLanguageTaggedLiteral").addAttribute("content",node.content).addAttribute("delimiter",node.delimiter).addAttribute("language",node.language)
+
+fun XMLElement.Companion.parseFromASTNode(node: ASTNode): XMLElement {
+    when {
+        node is ASTVar -> return XMLElement("ASTVar").addAttribute("name", node.name)
+        node is ASTAddition -> return XMLElement("ASTAddition").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTAggregation -> {
+            val res = XMLElement("ASTAggregation").addAttribute("type", "" + node.type).addAttribute("distinct", "" + node.distinct)
+            node.children.forEach() {
+                res.addContent(parseFromASTNode(it))
             }
-            throw Exception("parseFromASTNode for ${classNameToString(node)} undefined")
+            return res
+        }
+        node is ASTAnd -> {
+            val res = XMLElement("ASTAnd")
+            node.children.forEach() {
+                res.addContent(parseFromASTNode(it))
+            }
+            return res
+        }
+        node is ASTBuiltInCall -> {
+            val res = XMLElement("ASTBuiltInCall").addAttribute("function", "" + node.function)
+            node.children.forEach() {
+                res.addContent(parseFromASTNode(it))
+            }
+            return res
+        }
+        node is ASTDivision -> return XMLElement("ASTDivision").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTEQ -> return XMLElement("ASTEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTFunctionCall -> {
+            val res = XMLElement("ASTFunctionCall").addAttribute("iri", node.iri).addAttribute("distinct", "" + node.distinct)
+            return res
+        }
+        node is ASTGEQ -> return XMLElement("ASTGEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTGT -> return XMLElement("ASTGT").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTInteger -> return XMLElement("ASTInteger").addAttribute("value", "" + node.value)
+        node is ASTIn -> return XMLElement("ASTIn").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTIri -> return XMLElement("ASTIri").addAttribute("iri", node.iri)
+        node is ASTLEQ -> return XMLElement("ASTLEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTLT -> return XMLElement("ASTLT").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTMultiplication -> return XMLElement("ASTMultiplication").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTNotIn -> return XMLElement("ASTNotIn").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTNot -> return XMLElement("ASTNot").addContent(parseFromASTNode(node.children[0]))
+        node is ASTOr -> {
+            val res = XMLElement("ASTOr")
+            node.children.forEach() {
+                res.addContent(parseFromASTNode(it))
+            }
+            return res
+        }
+        node is ASTBooleanLiteral -> return XMLElement("ASTBooleanLiteral").addAttribute("value", "" + node.value)
+        node is ASTDecimal -> return XMLElement("ASTDecimal").addAttribute("value", "" + node.image)
+        node is ASTNEQ -> return XMLElement("ASTNEQ").addContent(XMLElement("childA").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childB").addContent(parseFromASTNode(node.children[1])))
+        node is ASTSet -> {
+            val res = XMLElement("ASTSet")
+            node.children.forEach() {
+                res.addContent(parseFromASTNode(it))
+            }
+            return res
+        }
+        node is ASTSimpleLiteral -> return XMLElement("ASTSimpleLiteral").addAttribute("content", node.content).addAttribute("delimiter", node.delimiter)
+        node is ASTTriple -> return XMLElement("ASTTriple").addContent(XMLElement("childS").addContent(parseFromASTNode(node.children[0]))).addContent(XMLElement("childP").addContent(parseFromASTNode(node.children[1]))).addContent(XMLElement("childO").addContent(parseFromASTNode(node.children[2])))
+        node is ASTTypedLiteral -> return XMLElement("ASTTypedLiteral").addAttribute("content", node.content).addAttribute("delimiter", node.delimiter).addAttribute("type_iri", node.type_iri)
+        node is ASTFilter -> return XMLElement("ASTFilter").addContent(parseFromASTNode(node.children[0]))
+        node is ASTLanguageTaggedLiteral -> return XMLElement("ASTLanguageTaggedLiteral").addAttribute("content", node.content).addAttribute("delimiter", node.delimiter).addAttribute("language", node.language)
+    }
+    throw Exception("parseFromASTNode for ${classNameToString(node)} undefined")
 }
--- src/commonMain/kotlin/lupos/s00misc/XMLElementToASTNode.kt
+++ src/commonMain/kotlin/lupos/s00misc/XMLElementToASTNode.kt
@@ -1,73 +1,75 @@
 package lupos.s00misc
-import lupos.s02buildSyntaxTree.sparql1_1.*
-fun XMLElement.Companion.toASTNode(node:XMLElement): ASTNode {
-when (node.tag) {
-                "ASTVar" -> return ASTVar(node.attributes["name"]!!)
-                "ASTAddition" -> return ASTAddition(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTAggregation"
-                -> {
-                    val childs = mutableListOf<ASTNode>()
-                    node.childs.forEach {
-                        childs.add(toASTNode(it))
-                    }
-                    return ASTAggregation(Aggregation.valueOf(node.attributes["type"]!!), node.attributes["distinct"]!!.toBoolean(), childs.toTypedArray())
-                }
-                "ASTAnd" -> {
-                    val childs = mutableListOf<ASTNode>()
-                    node.childs.forEach {
-                        childs.add(toASTNode(it))
-                    }
-                    return ASTAnd(childs.toTypedArray())
-                }
-                "ASTBuiltInCall" -> {
-                    val childs = mutableListOf<ASTNode>()
-                    node.childs.forEach {
-                        childs.add(toASTNode(it))
-                    }
-                    return ASTBuiltInCall(BuiltInFunctions.valueOf(node.attributes["function"]!!), childs.toTypedArray())
-                }
 
-                "ASTDivision" -> return ASTDivision(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTEQ" -> return ASTEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTFunctionCall" -> {
-                    val childs = mutableListOf<ASTNode>()
-                    node.childs.forEach {
-                        childs.add(toASTNode(it))
-                    }
-                    return ASTFunctionCall(node.attributes["iri"]!!, node.attributes["distinct"]!!.toBoolean(), childs.toTypedArray())
-                }
-                "ASTGEQ" -> return ASTGEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTGT" -> return ASTGT(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTInteger" -> return ASTInteger(node.attributes["value"]!!.toInt())
-                "ASTIn" -> return ASTIn(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTIri" -> return ASTIri(node.attributes["iri"]!!)
-                "ASTLEQ" -> return ASTLEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTLT" -> return ASTLT(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTMultiplication" -> ASTMultiplication(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTNotIn" -> return ASTNotIn(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-                "ASTNot" -> return ASTNot(toASTNode(node.childs.first()!!))
-                "ASTOr" -> {
-                    val childs = mutableListOf<ASTNode>()
-                    node.childs.forEach {
-                        childs.add(toASTNode(it))
-                    }
-                    return ASTOr(childs.toTypedArray())
-                }
-"ASTBooleanLiteral"->return ASTBooleanLiteral(node.attributes["value"]!!.toBoolean())
-"ASTDecimal"->return ASTDecimal(node.attributes["value"]!!)
-"ASTNEQ"->return ASTNEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
-"ASTSet"-> {
-         val childs = mutableListOf<ASTNode>()
-                    node.childs.forEach {
-                        childs.add(toASTNode(it))
-                    }
-                    return ASTSet(childs.toTypedArray())
-                }
-"ASTSimpleLiteral"->return ASTSimpleLiteral(node.attributes["content"]!!,node.attributes["delimiter"]!!)
-"ASTTriple"->return ASTTriple(toASTNode(node["childS"]!!), toASTNode(node["childP"]!!), toASTNode(node["childO"]!!))
-"ASTTypedLiteral"->return ASTTypedLiteral(node.attributes["content"]!!,node.attributes["delimiter"]!!,node.attributes["type_iri"]!!)
-"ASTFilter"->return ASTFilter(toASTNode(node.childs.first()!!))
-"ASTLanguageTaggedLiteral"->return ASTLanguageTaggedLiteral(node.attributes["content"]!!,node.attributes["delimiter"]!!,node.attributes["language"]!!)
+import lupos.s02buildSyntaxTree.sparql1_1.*
+
+fun XMLElement.Companion.toASTNode(node: XMLElement): ASTNode {
+    when (node.tag) {
+        "ASTVar" -> return ASTVar(node.attributes["name"]!!)
+        "ASTAddition" -> return ASTAddition(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTAggregation"
+        -> {
+            val childs = mutableListOf<ASTNode>()
+            node.childs.forEach {
+                childs.add(toASTNode(it))
             }
-            throw Exception("toASTNode for ${node.tag} undefined")
+            return ASTAggregation(Aggregation.valueOf(node.attributes["type"]!!), node.attributes["distinct"]!!.toBoolean(), childs.toTypedArray())
+        }
+        "ASTAnd" -> {
+            val childs = mutableListOf<ASTNode>()
+            node.childs.forEach {
+                childs.add(toASTNode(it))
+            }
+            return ASTAnd(childs.toTypedArray())
+        }
+        "ASTBuiltInCall" -> {
+            val childs = mutableListOf<ASTNode>()
+            node.childs.forEach {
+                childs.add(toASTNode(it))
+            }
+            return ASTBuiltInCall(BuiltInFunctions.valueOf(node.attributes["function"]!!), childs.toTypedArray())
+        }
+
+        "ASTDivision" -> return ASTDivision(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTEQ" -> return ASTEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTFunctionCall" -> {
+            val childs = mutableListOf<ASTNode>()
+            node.childs.forEach {
+                childs.add(toASTNode(it))
+            }
+            return ASTFunctionCall(node.attributes["iri"]!!, node.attributes["distinct"]!!.toBoolean(), childs.toTypedArray())
+        }
+        "ASTGEQ" -> return ASTGEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTGT" -> return ASTGT(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTInteger" -> return ASTInteger(node.attributes["value"]!!.toInt())
+        "ASTIn" -> return ASTIn(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTIri" -> return ASTIri(node.attributes["iri"]!!)
+        "ASTLEQ" -> return ASTLEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTLT" -> return ASTLT(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTMultiplication" -> ASTMultiplication(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTNotIn" -> return ASTNotIn(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTNot" -> return ASTNot(toASTNode(node.childs.first()!!))
+        "ASTOr" -> {
+            val childs = mutableListOf<ASTNode>()
+            node.childs.forEach {
+                childs.add(toASTNode(it))
+            }
+            return ASTOr(childs.toTypedArray())
+        }
+        "ASTBooleanLiteral" -> return ASTBooleanLiteral(node.attributes["value"]!!.toBoolean())
+        "ASTDecimal" -> return ASTDecimal(node.attributes["value"]!!)
+        "ASTNEQ" -> return ASTNEQ(toASTNode(node["childA"]!!), toASTNode(node["childB"]!!))
+        "ASTSet" -> {
+            val childs = mutableListOf<ASTNode>()
+            node.childs.forEach {
+                childs.add(toASTNode(it))
+            }
+            return ASTSet(childs.toTypedArray())
+        }
+        "ASTSimpleLiteral" -> return ASTSimpleLiteral(node.attributes["content"]!!, node.attributes["delimiter"]!!)
+        "ASTTriple" -> return ASTTriple(toASTNode(node["childS"]!!), toASTNode(node["childP"]!!), toASTNode(node["childO"]!!))
+        "ASTTypedLiteral" -> return ASTTypedLiteral(node.attributes["content"]!!, node.attributes["delimiter"]!!, node.attributes["type_iri"]!!)
+        "ASTFilter" -> return ASTFilter(toASTNode(node.childs.first()!!))
+        "ASTLanguageTaggedLiteral" -> return ASTLanguageTaggedLiteral(node.attributes["content"]!!, node.attributes["delimiter"]!!, node.attributes["language"]!!)
+    }
+    throw Exception("toASTNode for ${node.tag} undefined")
 }
