33dad90a22a9c513fe1e2acf2656a525f39cb9a8
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-02-17 10:33:03 +0100

    fix some bugs

--- src/commonS00ExecutionParallelMain/kotlin/lupos/s00misc/CoroutinesHelper.kt
+++ src/commonS00ExecutionParallelMain/kotlin/lupos/s00misc/CoroutinesHelper.kt
@@ -3,25 +3,28 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.*
 import kotlin.coroutines.*
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.sync.Mutex
 
 object CoroutinesHelper {
-
     val channelType=2
     fun run(action: suspend CoroutineScope.() -> Unit){
-	println("prepare on ${Thread.currentThread().name}")
 	GlobalScope.launch{
-		println("launched on ${Thread.currentThread().name}")
 		action()
-		println("finished on ${Thread.currentThread().name}")
 	}
-	println("finish on ${Thread.currentThread().name}")
     }
 
     inline fun runBlock(crossinline action: suspend CoroutineScope.() -> Unit) = runBlocking {
-println("blocking start on ${Thread.currentThread().name}")
 	action()
-println("blocking finished on ${Thread.currentThread().name}")
     }
+
+inline fun runBlockWithLock(lock:Mutex,crossinline action: () -> Unit)  =	runBlocking {
+		try{
+			lock.lock()
+			action()
+		}finally{
+			lock.unlock()
+		}
+}
 
 }
 
--- src/commonS03DictionaryIntArrayMain/kotlin/lupos/s03resultRepresentation/ResultSetDictionary.kt
+++ src/commonS03DictionaryIntArrayMain/kotlin/lupos/s03resultRepresentation/ResultSetDictionary.kt
@@ -1,22 +1,36 @@
 package lupos.s03resultRepresentation
 
+import kotlinx.coroutines.sync.Mutex
+import lupos.s00misc.*
+
 class ResultSetDictionary() {
     val mapSTL = mutableMapOf<String, Value>()
     val mapLTS = mutableListOf<String>()
     val undefValue = Value.MAX_VALUE
+    val mutex=Mutex()
     fun createValue(value: String): Value {
+var res:Value=undefValue
+CoroutinesHelper.runBlockWithLock(mutex,{
         val o = mapSTL[value]
         if (o != null)
-            return o
+            res= o
+else{
         val l = mapLTS.size
         mapSTL[value] = l
         mapLTS.add(value)
-        return l
-    }
+        res= l
+}
+    })
+return res
+}
 
     fun getValue(value: Value): String? {
         if (value == undefValue)
             return null
-        return mapLTS[value.toInt()]
-    }
+var res:String?=null
+ CoroutinesHelper.runBlockWithLock(mutex,{
+        res= mapLTS[value.toInt()]
+    })
+return res
+}
 }
