7171369a4237bbd761a552afbf49a23fa650e7da
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-02-22 17:31:23 +0100

    more tests

--- src/commonMain/kotlin/lupos/s04arithmetikOperators/singleinput/AOPBuildInCallDATATYPE.kt
+++ src/commonMain/kotlin/lupos/s04arithmetikOperators/singleinput/AOPBuildInCallDATATYPE.kt
@@ -25,8 +25,14 @@ class AOPBuildInCallDATATYPE(child: AOPBase) : AOPBase() {
 
     override fun calculate(resultSet: ResultSet, resultRow: ResultRow): AOPConstant {
         val a = (children[0] as AOPBase).calculate(resultSet, resultRow)
-        if (a is AOPTypedLiteral)
-            return addMicroTest(this, resultRow, resultSet, AOPSimpleLiteral(a.delimiter, a.type_iri))
+        when (a){
+        	is AOPTypedLiteral ->return addMicroTest(this, resultRow, resultSet, AOPIri(a.type_iri))
+		is AOPBoolean -> return addMicroTest(this, resultRow, resultSet, AOPIri( "http://www.w3.org/2001/XMLSchema#boolean"))
+		is AOPDateTime ->return addMicroTest(this, resultRow, resultSet, AOPIri( "http://www.w3.org/2001/XMLSchema#dateTime"))
+		is AOPDecimal->return addMicroTest(this, resultRow, resultSet, AOPIri( "http://www.w3.org/2001/XMLSchema#decimal"))
+		is AOPDouble->return addMicroTest(this, resultRow, resultSet, AOPIri( "http://www.w3.org/2001/XMLSchema#double"))
+		is AOPInteger->return addMicroTest(this, resultRow, resultSet, AOPIri( "http://www.w3.org/2001/XMLSchema#integer"))
+	}
         throw addMicroTest(this, resultRow, resultSet, Exception("AOPBuiltInCall DATATYPE only works with typed string input"))
     }
 }
