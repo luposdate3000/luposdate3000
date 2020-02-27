c72dfab03b49e1f6bb50dbb8ab5c6f8673dd89fe
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-02-27 15:07:01 +0100

    static optimisation of arithmetic operations

--- src/commonMain/kotlin/lupos/s08logicalOptimisation/LogicalOptimizerArithmetic.kt
+++ src/commonMain/kotlin/lupos/s08logicalOptimisation/LogicalOptimizerArithmetic.kt
@@ -4,7 +4,10 @@ import lupos.s00misc.EOperatorID
 import lupos.s00misc.EOptimizerID
 import lupos.s00misc.ExecuteOptimizer
 import lupos.s03resultRepresentation.ResultSetDictionary
+import lupos.s03resultRepresentation.ResultSet
 import lupos.s04arithmetikOperators.AOPBase
+import lupos.s04arithmetikOperators.noinput.AOPValue
+import lupos.s04arithmetikOperators.noinput.AOPUndef
 import lupos.s04logicalOperators.OPBase
 import lupos.s04logicalOperators.singleinput.modifiers.LOPPrefix
 import lupos.s08logicalOptimisation.OptimizerBase
@@ -13,9 +16,20 @@ import lupos.s08logicalOptimisation.OptimizerBase
 class LogicalOptimizerArithmetic(transactionID: Long, dictionary: ResultSetDictionary) : OptimizerBase(transactionID, dictionary, EOptimizerID.LogicalOptimizerArithmeticID) {
     override val classname = "LogicalOptimizerArithmetic"
     override fun optimize(node: OPBase, parent: OPBase?, onChange: () -> Unit) = ExecuteOptimizer.invoke({ this }, { node }, {
-        if (node is AOPBase && node.children.size > 0) {
+var res=node
+        if (node is AOPBase && node !is AOPValue&& node.children.size > 0 && node.getRequiredVariableNamesRecoursive().size==0) {
+val resultSet=ResultSet(ResultSetDictionary())
+val resultRow=resultSet.createResultRow()
+println("xxxLogicalOptimizerArithmetic")
+println(node)
+try{
+		res=node.calculate(resultSet,resultRow)
+}catch(e:Throwable){
+res=AOPUndef()
+}
+println(res)
             onChange()
         }
-        node
+        res
     })
 }
--- src/commonMain/kotlin/lupos/s08logicalOptimisation/LogicalOptimizerBindToFilter.kt
+++ src/commonMain/kotlin/lupos/s08logicalOptimisation/LogicalOptimizerBindToFilter.kt
@@ -1,13 +1,12 @@
 package lupos.s08logicalOptimisation
+import lupos.s04arithmetikOperators.noinput.AOPVariable
+import lupos.s04arithmetikOperators.multiinput.AOPEQ
+import lupos.s04arithmetikOperators.AOPBase
 
 import lupos.s00misc.EOperatorID
 import lupos.s00misc.EOptimizerID
 import lupos.s00misc.ExecuteOptimizer
 import lupos.s03resultRepresentation.ResultSetDictionary
-import lupos.s04arithmetikOperators.*
-import lupos.s04arithmetikOperators.multiinput.*
-import lupos.s04arithmetikOperators.noinput.*
-import lupos.s04arithmetikOperators.singleinput.*
 import lupos.s04logicalOperators.OPBase
 import lupos.s04logicalOperators.singleinput.LOPBind
 import lupos.s04logicalOperators.singleinput.LOPFilter
