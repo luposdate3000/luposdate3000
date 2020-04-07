2db07f64d3774d4bf2f583c1ed063cfcb7373053
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-04-07 17:28:36 +0200

    a

--- exec-benchmark-sp2b-graphic.sh
+++ exec-benchmark-sp2b-graphic.sh
@@ -27,6 +27,7 @@ plot \
  'tmp/$query.luposdate-009-9bfbbf63581ae66a3a94ea23a07f3ae5c9949e35-internal.csv' using 2:6 title "luposdate3000 009" with linespoints, \
  'tmp/$query.luposdate-010-6f542a3a5e5a84e248fa8d1da14cd92f90a7f586-internal.csv' using 2:6 title "luposdate3000 010" with linespoints, \
  'tmp/$query.luposdate-011-a054a02d2f1996d2464709e2fc6e701d33830f20-internal.csv' using 2:6 title "luposdate3000 011" with linespoints, \
+ 'tmp/$query.luposdate-012-1bbf7e5e728c3fbbb6dffe9ad421e86f738876d3-internal.csv' using 2:6 title "luposdate3000 012" with linespoints, \
  'tmp/$query.jena.csv' using 2:6 title "jena-with-curl" with linespoints
 EOF
 
