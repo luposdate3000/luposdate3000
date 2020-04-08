4661dc7cf4c9856a74cc1a446cd45f586aa39b29
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-04-08 14:03:10 +0200

    a

--- exec-benchmark-sp2b-graphic.sh
+++ exec-benchmark-sp2b-graphic.sh
@@ -19,10 +19,10 @@ set logscale y
 
 set title "$query"
 plot \
- 'tmp/$query.luposdate-008-c5a096ef9278d91c9372d567b076d6bef496172d-internal.csv' using 2:6 title "luposdate3000 008" with linespoints, \
- 'tmp/$query.luposdate-009-9bfbbf63581ae66a3a94ea23a07f3ae5c9949e35-internal.csv' using 2:6 title "luposdate3000 009" with linespoints, \
  'tmp/$query.luposdate-011-a054a02d2f1996d2464709e2fc6e701d33830f20-internal.csv' using 2:6 title "luposdate3000 011" with linespoints, \
  'tmp/$query.luposdate-012-1bbf7e5e728c3fbbb6dffe9ad421e86f738876d3-internal.csv' using 2:6 title "luposdate3000 012" with linespoints, \
+ 'tmp/$query.luposdate-013-f53095abed4033102e3393a2d35d4a1365531193-internal.csv' using 2:6 title "luposdate3000 013" with linespoints, \
+ 'tmp/$query.luposdate-014-9e86bb359e8003a1ff6f04c974f2683ad018e7b9-internal.csv' using 2:6 title "luposdate3000 014" with linespoints, \
  'tmp/$query.jena.csv' using 2:6 title "jena-with-curl" with linespoints
 EOF
 
