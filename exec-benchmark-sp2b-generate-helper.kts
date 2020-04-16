66c73717038f0b93c31323dbef15ce0606317b7c
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-04-16 09:28:03 +0200

    a

--- exec-benchmark-sp2b-generate-helper.kts
+++ exec-benchmark-sp2b-generate-helper.kts
@@ -1,8 +1,9 @@
 #!/bin/kscript
 import java.io.*
 
+var compress = false
 var folderName = args[0]
-var line = readLine()
+var line2 = readLine()
 var chunk = 0
 var outsize = 0
 var out: PrintWriter? = null
@@ -11,21 +12,22 @@ var currentTriple = Array<String>(3) { "" }
 var lastTriple = Array<String>(3) { "" }
 var nextType = 0 /* 0:S,1:P,2:O*/
 var finishTriple = false
-while (line != null) {
+while (line2 != null) {
+    val line = line2!!
     if (line.startsWith("@prefix")) {
         prefixes.add(line)
     } else {
-        out = nextChunk()
+        nextChunk()
         var column = 0
         var start = column
         while (column < line.length) {
-            while (column < line.length && line.charAt(column) == ' ') {
+            while (column < line.length && line[column] == ' ') {
                 column++
             }
             start = column
             var qouteCount = 0
-            while (column < line.length) {
-                when (line.charAt(column)) {
+            loop@ while (column < line.length) {
+                when (line[column]) {
                     '"' -> qouteCount++
                     '<' -> {
                         if (qouteCount % 2 == 0) {
@@ -39,12 +41,12 @@ while (line != null) {
                     }
                     ' ' -> {
                         if (qouteCount % 2 == 0) {
-                            break
+                            break@loop
                         }
                     }
                     '.', ',', ';' -> {
                         if (qouteCount % 2 == 0 && nextType == 2) {
-                            break
+                            break@loop
                         }
                     }
                 }
@@ -53,17 +55,17 @@ while (line != null) {
             currentTriple[nextType] = line.substring(start, column)
             if (nextType == 2) {
                 writeTriple()
-                while (column < line.length && line.charAt(column) == ' ') {
+                while (column < line.length && line[column] == ' ') {
                     column++
                 }
-                when (line.charAt(column)) {
-                    "." -> {
+                when (line[column]) {
+                    '.' -> {
                         nextType = 0
                     }
-                    ";" -> {
+                    ';' -> {
                         nextType = 1
                     }
-                    "," -> {
+                    ',' -> {
                         nextType = 2
                     }
                     else -> {
@@ -76,41 +78,51 @@ while (line != null) {
             }
         }
     }
-    line = readLine()
+    line2 = readLine()
 }
+finishChunk()
 fun writeTriple() {
-    finishTriple = true
-    if (lastTriple[0] == "") {
-        write(currentTriple[0] + " " + currentTriple[1] + " " + currentTriple[2] + " ")
-    } else {
-        if (currentTriple[0] == lastTriple[0]) {
-            if (currentTriple[1] == lastTriple[1]) {
-                write(",\n" + currentTriple[2] + " ")
-            } else {
-                write(";\n" + currentTriple[1] + " " + currentTriple[2] + " ")
-            }
+    if (compress) {
+        finishTriple = true
+        if (lastTriple[0] == "") {
+            write(currentTriple[0] + " " + currentTriple[1] + " " + currentTriple[2] + " ")
         } else {
-            write(".\n" + currentTriple[0] + " " + currentTriple[1] + " " + currentTriple[2] + " ")
+            if (currentTriple[0] == lastTriple[0]) {
+                if (currentTriple[1] == lastTriple[1]) {
+                    write(",\n" + currentTriple[2] + " ")
+                } else {
+                    write(";\n" + currentTriple[1] + " " + currentTriple[2] + " ")
+                }
+            } else {
+                write(".\n" + currentTriple[0] + " " + currentTriple[1] + " " + currentTriple[2] + " ")
+            }
         }
-    }
-    for (i in 0 until 3) {
-        lastTriple[i] = currentTriple[i]
+        for (i in 0 until 3) {
+            lastTriple[i] = currentTriple[i]
+        }
+    } else {
+        write(currentTriple[0] + " " + currentTriple[1] + " " + currentTriple[2] + " .\n")
     }
 }
 
 fun write(str: String) {
-    out.print(str)
+    out!!.print(str)
     outsize += str.length
+}
+
+fun finishChunk() {
+    if (out != null) {
+        if (finishTriple) {
+            out!!.println(".")
+        }
+        out!!.close()
+    }
+    outsize = 0
 }
 
 fun nextChunk() {
     if (outsize > 1000000000 || out == null) {
-        if (out != null) {
-            if (finishTriple) {
-                out.println(".")
-            }
-            out.close()
-        }
+        finishChunk()
         out = File(folderName + "/data" + chunk++ + ".n3").printWriter()
         for (p in prefixes) {
             write(p + "\n")
