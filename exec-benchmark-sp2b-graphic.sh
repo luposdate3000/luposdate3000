c3b514932a547874ad52c35727a150b4abdf9fb1
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-04-21 12:06:35 +0200

    a

--- exec-benchmark-sp2b-graphic.sh
+++ exec-benchmark-sp2b-graphic.sh
@@ -4,7 +4,7 @@ for query in $(cat benchmark_results/sp2b/*.csv | sed "s/sparql.*/sparql/g" | so
 do
 for f in $(find benchmark_results/sp2b -name "*.csv")
 do
-target=$(echo $f | sed "s-benchmark_results/sp2b/-tmp/$query.-g")
+target=$(echo $f | sed "s#benchmark_results/sp2b/#tmp/$query.#g")
 cat $f  | grep $query > $target
 done
 
@@ -24,7 +24,8 @@ plot \
  'tmp/$query.luposdate-013-f53095abed4033102e3393a2d35d4a1365531193-internal.csv' using 2:6 title "luposdate3000 013" with linespoints, \
  'tmp/$query.luposdate-014-9e86bb359e8003a1ff6f04c974f2683ad018e7b9-internal.csv' using 2:6 title "luposdate3000 014" with linespoints, \
  'tmp/$query.luposdate-015-9318ad8651080e14aede8ca36231969b6a8e0c20-internal.csv' using 2:6 title "luposdate3000 015" with linespoints, \
- 'tmp/$query.luposdate-b2841382ba6cfa5475c773f699e58c3937e14c35-internal.csv' using 2:6 title "luposdate3000 016" with linespoints, \
+ 'tmp/$query.luposdate-016-b2841382ba6cfa5475c773f699e58c3937e14c35-internal.csv' using 2:6 title "luposdate3000 016" with linespoints, \
+ 'tmp/$query.luposdate-b3be67fb75117794500a208aa3b5c700e411b994-internal.csv' using 2:6 title "luposdate3000 017" with linespoints, \
  'tmp/$query.jena-internal.csv' using 2:6 title "jena" with linespoints
 EOF
 
