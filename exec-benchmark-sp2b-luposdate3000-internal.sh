04b55ddcfe11bef1684ce796933224ca7f82ce83
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-05-13 05:25:18 +0200

    benchmark

--- exec-benchmark-sp2b-luposdate3000-internal.sh
+++ exec-benchmark-sp2b-luposdate3000-internal.sh
@@ -8,9 +8,9 @@ mkdir -p log/benchtmp
 
 ./generate-buildfile.kts 1.4.255-SNAPSHOT jvm Benchmark Off Fast Sequential Heap MultiMap BPlusTree Dummy Korio None Local Off BTree BTree None Empty 8196 1024 512 512
 ./tool-gradle-build.sh
-ln -s $(readlink -f build/executable) log/benchtmp/Single_BTree_Empty.x
+ln -s $(readlink -f build/executable) log/benchtmp/Multi_BPlusTree_Empty.x
 
-versions=( "Single_BTree_Empty" )
+versions=( "Multi_BPlusTree_Empty" )
 
 p=$(pwd)/benchmark_results/sp2b
 mkdir -p $p
@@ -44,9 +44,9 @@ do
 		then
 			if [ $i -eq 0 ]
 			then
-				./log/benchtmp/$version.x "IMPORT" "$triplesfolder/data" "$(find $triplesfolder/*.n3 | paste -s -d ';')" "$queries" "10" "$triples" "$size" > log/benchtmp/x
+				./log/benchtmp/$version.x "IMPORT" "$triplesfolder/data" "$(find $triplesfolder/*.n3 | paste -s -d ';')" "$queries" "30" "$triples" "$size" > log/benchtmp/x
 			else
-				./log/benchtmp/$version.x "LOAD" "$triplesfolder/data" "" "$queries" "10" "$triples" "$size" > log/benchtmp/x
+				./log/benchtmp/$version.x "LOAD" "$triplesfolder/data" "" "$queries" "30" "$triples" "$size" > log/benchtmp/x
 			fi
 			cat log/benchtmp/x | grep "sparql,$triples," >> $p/luposdate-$version-$(git rev-parse HEAD)-internal.csv
 			cat log/benchtmp/x | grep "sparql,$triples," | grep -v "sparql,$triples,0,.," | sed "s/,.*//" > log/benchtmp/$version.queries
