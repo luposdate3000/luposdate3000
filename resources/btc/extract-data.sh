8cdc102007585532cd0f535c337abedbba4fb236
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-06-04 18:57:19 +0200

    a

--- resources/btc/extract-data.sh
+++ resources/btc/extract-data.sh
@@ -27,24 +27,33 @@ do
 		(
 			LC_ALL=C
 			echo "start with $i"
-			parallel -a /src/luposdate3000/btc-2019-triples.nt --pipepart grep -F "'$i'"  > $f
+			parallel -a /src/luposdate3000/btc-2019-triples.nt --pipepart grep -f "'$i'"  > $f
 			echo "done with $i"
 		)
 	fi
 done
 wait
 
+rm config.csv2
 for f in $(find . -name "*.sparql" | sort)
 do
-	rm $f.n2
-	for x in $(cat $f | tr " " "\n" | sort | uniq | grep -v "^?" | sort)
-	do
-		i="data$(echo $x | sed 's/[^a-zA-Z0-9]/_/g').n3"
-		echo $f $i
-		cat $i >> $f.n2
-	done
-	sort -u $f.n2 > $f.n3
-	rm $f.n2
+	if [ -f "$f.n3" ]
+	then
+		echo "$f.n3 exist"
+	else
+		rm $f.n2
+		for x in $(cat $f | tr " " "\n" | sort | uniq | grep -v "^?" | sort)
+		do
+			i="data$(echo $x | sed 's/[^a-zA-Z0-9]/_/g').n3"
+			echo $f $i
+			cat $i >> $f.n2
+		done
+		sort -u $f.n2 > $f.n3
+		rm $f.n2
+	fi
+	echo $(wc -l $f.n3),$f,$f.n3,$f.srx >> config.csv2
 done
 
 rm x
+cat config.csv2 | sort -n | uniq > config.csv
+rm config.csv2
