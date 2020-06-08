6de50b4290cb0995bf67533206868259b7831897
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-06-08 06:59:03 +0200

    a

--- exec-benchmark-graphic.sh
+++ exec-benchmark-graphic.sh
@@ -28,6 +28,8 @@ set logscale x
 set logscale y
 set title "$query"
 plot \
+ 'tmp/$query.luposdate-028-Multi_BPlusTree_Empty-2d6131517f75828a200dce7012453f71b09adc14-internal.csv' using 2:6 title "luposdate3000 028" with linespoints, \
+ 'tmp/$query.luposdate-029-Multi_BPlusTree_Empty-30e88f5f5f449b2300e69aaee0f89194a666f54d-internal.csv' using 2:6 title "luposdate3000 029" with linespoints, \
  'tmp/$query.luposdate-030-Multi_BPlusTree_Empty-d91c0c41b380ec436db7a4bb677d1d5d6709c127-internal.csv' using 2:6 title "luposdate3000 030" with linespoints, \
  'tmp/$query.jena-Multi_BPlusTree_Empty-cf220df24c808d908415a94727a6e4b8bc8a7554-internal.csv' using 2:6 title "jena" with linespoints
 EOF
