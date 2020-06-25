557e17b0b2991ce3dc7c1f5998da1eb08cfbd486
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-06-25 07:53:17 +0000

    dont inline during tests ... this breaks the exception stack trace

--- src/commonMain/kotlin/lupos/s00misc/SanityCheckOff.kt
+++ src/commonMain/kotlin/lupos/s00misc/SanityCheckOff.kt
@@ -3,9 +3,9 @@ package lupos.s00misc
 import lupos.s00misc.Coverage
 
 object SanityCheckOff {
-    inline fun <T> helper(action: () -> Unit): T? = null
-    operator fun invoke(action: () -> Unit) {}
-    inline fun check(value: () -> Boolean, msg: () -> String) {}
-    inline fun check(value: () -> Boolean) {}
+ inline   operator fun invoke(crossinline action: () -> Unit) {}
+    inline fun <T> helper(crossinline action: () -> Unit): T? = null
+    inline fun check(crossinline value: () -> Boolean,crossinline msg: () -> String) {}
+    inline fun check(crossinline value: () -> Boolean) {}
     inline fun checkUnreachable(): Nothing = throw UnreachableException()
 }
--- src/commonMain/kotlin/lupos/s00misc/SanityCheckOn.kt
+++ src/commonMain/kotlin/lupos/s00misc/SanityCheckOn.kt
@@ -3,9 +3,9 @@ package lupos.s00misc
 import lupos.s00misc.Coverage
 
 object SanityCheckOn {
-    operator fun invoke(action: () -> Unit) = action()
-    inline fun <T> helper(action: () -> T): T? = action()
-    inline fun check(value: () -> Boolean, msg: () -> String) = require(value(), msg)
-    inline fun check(value: () -> Boolean) = require(value())
-    inline fun checkUnreachable(): Nothing = throw UnreachableException()
+    operator fun invoke(/*crossinline*/action: () -> Unit) = action()
+    /*inline*/ fun <T> helper(/*crossinline*/action: () -> T): T? = action()
+    /*inline*/ fun check(/*crossinline*/value: () -> Boolean,/*crossinline*/ msg: () -> String) = require(value(), msg)
+    /*inline*/ fun check(/*crossinline*/value: () -> Boolean) = require(value())
+    /*inline*/ fun checkUnreachable(): Nothing = throw UnreachableException()
 }
