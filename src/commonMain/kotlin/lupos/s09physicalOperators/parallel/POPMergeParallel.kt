605659a8cee7b6b3f8d39297b0fcd73c2136f7f6
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-07-09 10:02:50 +0000

    fix some bugs - but not all. seems to run for many but not all tests

--- src/commonMain/kotlin/lupos/s09physicalOperators/parallel/POPMergeParallel.kt
+++ src/commonMain/kotlin/lupos/s09physicalOperators/parallel/POPMergeParallel.kt
@@ -1,12 +1,15 @@
 package lupos.s09physicalOperators.parallel
 
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.yield
+import kotlinx.coroutines.delay
 import lupos.s00misc.Coverage
 import lupos.s00misc.EOperatorID
 import lupos.s00misc.ESortPriority
@@ -31,8 +34,8 @@ class POPMergeParallel(query: Query, projectedVariables: List<String>, val parti
     }
 
     override fun cloneOP() = POPMergeParallel(query, projectedVariables, partitionVariable, children[0].cloneOP())
-    override fun toSparql() = children[0].toSparql()                                                     
-    override fun equals(other: Any?): Boolean = other is POPSplitParallel && children[0] == other.children[0] && partitionVariable==other.partitionVariable
+    override fun toSparql() = children[0].toSparql()
+    override fun equals(other: Any?): Boolean = other is POPMergeParallel && children[0] == other.children[0] && partitionVariable == other.partitionVariable
     override suspend fun evaluate(parent: Partition): IteratorBundle {
         if (ParallelBase.k == 1) {
             //single partition - just pass through
@@ -46,60 +49,66 @@ class POPMergeParallel(query: Query, projectedVariables: List<String>, val parti
             val elementsPerRing = ParallelBase.queue_size * variables.size
             val ringbuffer = IntArray(elementsPerRing * ParallelBase.k) //only modified by writer, reader just modifies its pointer
             val ringbufferStart = IntArray(ParallelBase.k) { it * elementsPerRing } //constant
-            val ringbufferReadHead = IntArray(ParallelBase.k) { it * elementsPerRing } //owned by read-thread - no locking required
-            val ringbufferWriteHead = IntArray(ParallelBase.k) { it * elementsPerRing } //owned by write thread - no locking required
+            val ringbufferReadHead = IntArray(ParallelBase.k) { 0 } //owned by read-thread - no locking required
+            val ringbufferWriteHead = IntArray(ParallelBase.k) { 0 } //owned by write thread - no locking required
             val writerFinished = IntArray(ParallelBase.k) { 0 } //writer changes to 1 if finished
             var readerFinished = 0
             val jobs = mutableListOf<Job>()
             for (p in 0 until ParallelBase.k) {
-                runBlocking {
-                    val job = launch {
-                        val child = children[0].evaluate(Partition(parent, partitionVariable, p)).rows
-                        val variableMapping = IntArray(variables.size)
-                        for (variable in 0 until variables.size) {
-                            for (variable2 in 0 until variables.size) {
-                                if (variables[variable2] == child.columns[variable]) {
-                                    variableMapping[variable] = variable2
-                                    break
-                                }
-                            }
-                        }
-                        loop@ while (isActive) {
-                            var t = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
-                            while (ringbufferReadHead[p] == t) {
-                                yield()
-                                if (!isActive) {
-                                    child.close()
-                                    writerFinished[p] = 1
-                                    break@loop
-                                }
-                            }
-                            var tmp = child.next()
-                            if (tmp == -1) {
-                                writerFinished[p] = 1
-                                break@loop
-                            } else {
-                                for (variable in 0 until variables.size) {
-                                    ringbuffer[ringbufferWriteHead[p] + variableMapping[variable] + ringbufferStart[p]] = child.buf[tmp + variable]
-                                }
-                                ringbufferWriteHead[p] = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
+                val job = GlobalScope.launch(Dispatchers.Default) {
+                    val child = children[0].evaluate(Partition(parent, partitionVariable, p)).rows
+                    val variableMapping = IntArray(variables.size)
+                    for (variable in 0 until variables.size) {
+                        for (variable2 in 0 until variables.size) {
+                            if (variables[variable2] == child.columns[variable]) {
+                                variableMapping[variable] = variable2
+                                break
                             }
                         }
                     }
-                    jobs.add(job)
+                    loop@ while (isActive) {
+                        println("merge $uuid $p writer loop start")
+                        var t = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
+                        while (ringbufferReadHead[p] == t) {
+                            println("merge $uuid $p writer wait for reader to remove data")
+                            delay(1)
+                            if (!isActive) {
+println("merge $uuid $p writer closed A")
+                                child.close()
+                                writerFinished[p] = 1
+                                break@loop
+                            }
+                        }
+                        var tmp = child.next()
+                        if (tmp == -1) {
+println("merge $uuid $p writer closed B")
+                            writerFinished[p] = 1
+                            break@loop
+                        } else {
+                            println("merge $uuid $p writer append data")
+                            for (variable in 0 until variables.size) {
+                                ringbuffer[ringbufferWriteHead[p] + variableMapping[variable] + ringbufferStart[p]] = child.buf[tmp + variable]
+                            }
+                            ringbufferWriteHead[p] = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
+                        }
+                    }
+println("merge $uuid $p writer exited loop")
                 }
+                jobs.add(job)
             }
             var iterator = RowIterator()
             iterator.columns = variables.toTypedArray()
             iterator.next = {
                 var res = -1
                 loop@ while (true) {
+                    println("merge $uuid reader loop start")
                     var finishedWriters = 0
                     for (p in 0 until ParallelBase.k) {
                         if (writerFinished[p] == 1) {
                             finishedWriters++
                         } else if (ringbufferReadHead[p] != ringbufferWriteHead[p]) {
                             //non empty queue -> read one row
+                            println("merge $uuid $p reader consumed data")
                             for (variable in 0 until variables.size) {
                                 iterator.buf[variable] = (ringbuffer[ringbufferReadHead[p] + variable + ringbufferStart[p]])
                             }
@@ -112,11 +121,13 @@ class POPMergeParallel(query: Query, projectedVariables: List<String>, val parti
                         //done
                         break@loop
                     }
-                    yield()
+                    println("merge $uuid reader wait for writer")
+                    delay(1)
                 }
                 /*return*/res
             }
             iterator.close = {
+                println("merge $uuid reader closed")
                 readerFinished = 1
                 runBlocking {
                     for (job in jobs) {
--- src/commonMain/kotlin/lupos/s09physicalOperators/parallel/POPSplitParallel.kt
+++ src/commonMain/kotlin/lupos/s09physicalOperators/parallel/POPSplitParallel.kt
@@ -4,9 +4,12 @@ import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.yield
+import kotlinx.coroutines.delay
 import lupos.s00misc.Coverage
 import lupos.s00misc.EOperatorID
 import lupos.s00misc.ESortPriority
@@ -31,8 +34,8 @@ class POPSplitParallel(query: Query, projectedVariables: List<String>, val parti
     }
 
     override fun cloneOP() = POPSplitParallel(query, projectedVariables, partitionVariable, children[0].cloneOP())
-    override fun toSparql() = children[0].toSparql() 
-    override fun equals(other: Any?): Boolean = other is POPSplitParallel && children[0] == other.children[0] && partitionVariable==other.partitionVariable
+    override fun toSparql() = children[0].toSparql()
+    override fun equals(other: Any?): Boolean = other is POPSplitParallel && children[0] == other.children[0] && partitionVariable == other.partitionVariable
     override suspend fun evaluate(parent: Partition): IteratorBundle {
         if (ParallelBase.k == 1) {
             //single partition - just pass through
@@ -40,16 +43,16 @@ class POPSplitParallel(query: Query, projectedVariables: List<String>, val parti
         } else {
             var iterators: Array<IteratorBundle>? = null
             var job: Job? = null
-val childPartition=Partition(parent, partitionVariable)
+            val childPartition = Partition(parent, partitionVariable)
             query.partitionsLock.withWriteLockSuspend {
-val tmpIterators=query.partitionsIterators[uuid]
-if(tmpIterators!=null){
-                iterators = tmpIterators[childPartition]
-}
-val tmpJob=query.partitionsJobs[uuid]
-if(tmpJob!=null){
-                job = tmpJob[childPartition]
-}
+                val tmpIterators = query.partitionsIterators[uuid]
+                if (tmpIterators != null) {
+                    iterators = tmpIterators[childPartition]
+                }
+                val tmpJob = query.partitionsJobs[uuid]
+                if (tmpJob != null) {
+                    job = tmpJob[childPartition]
+                }
                 if (iterators == null) {
                     iterators = Array(ParallelBase.k) { IteratorBundle(0) }
                     val variables = getProvidedVariableNames()
@@ -60,65 +63,78 @@ if(tmpJob!=null){
                     val elementsPerRing = ParallelBase.queue_size * variables.size
                     val ringbuffer = IntArray(elementsPerRing * ParallelBase.k) //only modified by writer, reader just modifies its pointer
                     val ringbufferStart = IntArray(ParallelBase.k) { it * elementsPerRing } //constant
-                    val ringbufferReadHead = IntArray(ParallelBase.k) { it * elementsPerRing } //owned by read-thread - no locking required
-                    val ringbufferWriteHead = IntArray(ParallelBase.k) { it * elementsPerRing } //owned by write thread - no locking required
+                    val ringbufferReadHead = IntArray(ParallelBase.k) { 0 } //owned by read-thread - no locking required
+                    val ringbufferWriteHead = IntArray(ParallelBase.k) { 0 } //owned by write thread - no locking required
                     val readerFinished = IntArray(ParallelBase.k) { 0 } //writer changes to 1 if finished
                     var writerFinished = 0
-                    runBlocking {
-                        job = launch {
-                            val child = children[0].evaluate(childPartition).rows
-                            var hashVariableIndex = -1
-                            val variableMapping = IntArray(variables.size)
-                            for (variable in 0 until variables.size) {
-                                for (variable2 in 0 until variables.size) {
-                                    if (child.columns[variable] == partitionVariable) {
-                                        hashVariableIndex = variable
-                                    }
-                                    if (variables[variable2] == child.columns[variable]) {
-                                        variableMapping[variable] = variable2
-                                        break
-                                    }
+println("ringbuffersize = ${ringbuffer.size} ${elementsPerRing} ${ParallelBase.k} ${ringbufferStart.map{it}} ${ringbufferReadHead.map{it}} ${ringbufferWriteHead.map{it}}")
+                    job = GlobalScope.launch(Dispatchers.Default) {
+                        val child = children[0].evaluate(childPartition).rows
+                        var hashVariableIndex = -1
+                        val variableMapping = IntArray(variables.size)
+                        for (variable in 0 until variables.size) {
+                            for (variable2 in 0 until variables.size) {
+                                if (child.columns[variable] == partitionVariable) {
+                                    hashVariableIndex = variable
                                 }
-                            }
-                            SanityCheck.check { hashVariableIndex != -1 }
-                            loop@ while (isActive) {
-                                var tmp = child.next()
-                                if (tmp == -1) {
-                                    writerFinished = 1
-                                    break@loop
-                                } else {
-                                    var p = child.buf[tmp + hashVariableIndex]
-                                    if (p < 0) {
-                                        p = -p
-                                    }
-                                    p = p % ParallelBase.k
-                                    var t = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
-                                    while (ringbufferReadHead[p] == t) {
-                                        yield()
-                                        if (!isActive) {
-                                            child.close()
-                                            writerFinished = 1
-                                            break@loop
-                                        }
-                                    }
-                                    for (variable in 0 until variables.size) {
-                                        ringbuffer[ringbufferWriteHead[p] + variableMapping[variable] + ringbufferStart[p]] = child.buf[tmp + variable]
-                                    }
-                                    ringbufferWriteHead[p] = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
+                                if (variables[variable2] == child.columns[variable]) {
+                                    variableMapping[variable] = variable2
+                                    break
                                 }
                             }
                         }
+                        SanityCheck.check { hashVariableIndex != -1 }
+                        loop@ while (isActive) {
+                            println("split $uuid writer loop start")
+                            var tmp = child.next()
+                            if (tmp == -1) {
+                                println("split $uuid writer closed A")
+                                writerFinished = 1
+                                break@loop
+                            } else {
+                                var p = child.buf[tmp + hashVariableIndex]
+                                if (p < 0) {
+                                    p = -p
+                                }
+                                p = p % ParallelBase.k
+                                var t = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
+                                while (ringbufferReadHead[p] == t) {
+                                    println("split $uuid $p writer wait for reader")
+                                    delay(1)
+                                    if (!isActive) {
+                                println("split $uuid $p writer closed B")
+                                        child.close()
+                                        writerFinished = 1
+                                        break@loop
+                                    }
+                                }
+                                println("split $uuid $p writer append data ${variables.size} ${variableMapping.toMutableList()} ${ ringbufferStart[p]}")
+                                for (variable in 0 until variables.size) {
+println("split $uuid $p writer append data ... ${variable} ${ringbufferWriteHead[p] + variableMapping[variable] + ringbufferStart[p]} ${tmp + variable}")
+                                    ringbuffer[ringbufferWriteHead[p] + variableMapping[variable] + ringbufferStart[p]] = child.buf[tmp + variable]
+                                }
+                                println("split $uuid $p writer append data - written data")
+                                ringbufferWriteHead[p] = (ringbufferWriteHead[p] + variables.size) % elementsPerRing
+                                println("split $uuid $p writer append data - increased pointer")
+                            }
+println("split $uuid writer loop end of iteration")
+                        }
+println("split $uuid writer exited loop")
                     }
                     for (p in 0 until ParallelBase.k) {
                         var iterator = RowIterator()
                         iterator.columns = variables.toTypedArray()
+			iterator.buf=IntArray(variables.size)
                         iterator.next = {
                             var res = -1
                             loop@ while (true) {
+                                println("split $uuid $p reader loop start")
                                 if (writerFinished == 1) {
+println("split $uuid $p reader closed")
                                     break@loop
                                 } else if (ringbufferReadHead[p] != ringbufferWriteHead[p]) {
                                     //non empty queue -> read one row
+                                    println("split $uuid $p reader consumed data")
                                     for (variable in 0 until variables.size) {
                                         iterator.buf[variable] = (ringbuffer[ringbufferReadHead[p] + variable + ringbufferStart[p]])
                                     }
@@ -126,11 +142,13 @@ if(tmpJob!=null){
                                     ringbufferReadHead[p] = (ringbufferReadHead[p] + variables.size) % elementsPerRing
                                     break@loop
                                 }
-                                yield()
+                                println("split $uuid $p reader wait for writer")
+                                delay(1)
                             }
                             /*return*/res
                         }
                         iterator.close = {
+                            println("split $uuid $p reader close")
                             readerFinished[p] = 1
                             var tmp = 0
                             for (p in 0 until ParallelBase.k) {
@@ -143,16 +161,17 @@ if(tmpJob!=null){
                                     job!!.cancelAndJoin()
                                 }
                             }
+                            println("split $uuid $p reader closed")
                         }
                         iterators!![p] = IteratorBundle(iterator)
                     }
-if(tmpIterators==null||tmpJob==null){
-query.partitionsIterators[uuid]=mutableMapOf(childPartition to iterators!!)
-query.partitionsJobs[uuid]=mutableMapOf(childPartition to job!!)
-}else{
-                    tmpIterators[childPartition] = iterators!!
-                    tmpJob[childPartition] = job!!
-}
+                    if (tmpIterators == null || tmpJob == null) {
+                        query.partitionsIterators[uuid] = mutableMapOf(childPartition to iterators!!)
+                        query.partitionsJobs[uuid] = mutableMapOf(childPartition to job!!)
+                    } else {
+                        tmpIterators[childPartition] = iterators!!
+                        tmpJob[childPartition] = job!!
+                    }
                 }
             }
             return iterators!![parent.data[partitionVariable]!!]
