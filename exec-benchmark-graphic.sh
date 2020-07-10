05dc4f2742a8b28dd504508ddd6b3ee00a5ac508
Benjamin Warnke <benjamin.warnke@ifis.uni-luebeck.de>
2020-07-10 11:23:29 +0000

    benchmarks results ... noticed too late, that some debugging-code is present in release build - results are considered to be lower-bound

--- exec-benchmark-graphic.sh
+++ exec-benchmark-graphic.sh
@@ -8,13 +8,10 @@ for query in $(cat benchmark_results/$benchmark/*.csv | sed "s#sparql.*#sparql#g
 do
 echo $query
 	for f in $(find benchmark_results/$benchmark -name "*.csv" | grep \
-			-e cf220df24c808d908415a94727a6e4b8bc8a7554 \
 			-e f17068c716e93706cc5ae72fa050685a187888ea \
 			\
-			-e d91c0c41b380ec436db7a4bb677d1d5d6709c127 \
-			-e 2693848df72a5dc1fa17d5d85cf561fb6e88db9d \
-			-e 04d2c408e9542bc8d4b3840c2b1a3e3b0f7e01a5 \
-			-e 43a53e32c958efbd338747265b5d9877784b91fe
+			-e 43a53e32c958efbd338747265b5d9877784b91fe \
+			-e 6b716f9724074a0f1c0abdd5b0374ed68c9e3f14
 		)
 	do
 		target=$(echo $f | sed "s#benchmark_results/$benchmark/#tmp/$query.#g")
@@ -36,12 +33,9 @@ set logscale x
 set logscale y
 set title "$query"
 plot \
- 'tmp/$query.luposdate-030-Multi_BPlusTree_Empty-d91c0c41b380ec436db7a4bb677d1d5d6709c127-internal.csv' using 2:6 title "luposdate3000 030" with linespoints, \
- 'tmp/$query.luposdate-031-Multi_BPlusTree_XML-2693848df72a5dc1fa17d5d85cf561fb6e88db9d-internal.csv' using 2:6 title "luposdate3000 031 XML" with linespoints, \
- 'tmp/$query.luposdate-032-Multi_BPlusTree_Empty-04d2c408e9542bc8d4b3840c2b1a3e3b0f7e01a5-internal.csv' using 2:6 title "luposdate3000 032" with linespoints, \
- 'tmp/$query.luposdate3000-Multi_BPlusTree_Empty-43a53e32c958efbd338747265b5d9877784b91fe-internal.csv' using 2:6 title "luposdate3000 033" with linespoints, \
- 'tmp/$query.jena-Multi_BPlusTree_Empty-cf220df24c808d908415a94727a6e4b8bc8a7554-internal.csv' using 2:6 title "jena old" with linespoints, \
- 'tmp/$query.jena-Multi_BPlusTree_Empty-f17068c716e93706cc5ae72fa050685a187888ea-internal.csv' using 2:6 title "jena new" with linespoints
+ 'tmp/$query.luposdate-033-Multi_BPlusTree_Empty-43a53e32c958efbd338747265b5d9877784b91fe-internal.csv' using 2:6 title "luposdate3000 033" with linespoints, \
+ 'tmp/$query.luposdate3000-Multi_BPlusTree_Empty-6b716f9724074a0f1c0abdd5b0374ed68c9e3f14-internal.csv' using 2:6 title "luposdate3000 034" with linespoints, \
+ 'tmp/$query.jena-Multi_BPlusTree_Empty-f17068c716e93706cc5ae72fa050685a187888ea-internal.csv' using 2:6 title "jena" with linespoints
 EOF
 done
 mv tmp/*.png benchmark_results/
