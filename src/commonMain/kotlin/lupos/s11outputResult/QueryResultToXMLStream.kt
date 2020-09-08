bf68850b7599b66e0e2511ea442d461a84c0c635
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2020-09-08 12:10:14 +0000

    a

--- src/commonMain/kotlin/lupos/s11outputResult/QueryResultToXMLStream.kt
+++ src/commonMain/kotlin/lupos/s11outputResult/QueryResultToXMLStream.kt
@@ -2,6 +2,7 @@ package lupos.s11outputResult
 
 import lupos.s00misc.DateHelper
 import java.io.PrintWriter
+import java.io.StringWriter
 import lupos.s00misc.MyMapIntInt
 import lupos.s00misc.Partition
 import lupos.s00misc.SanityCheck
@@ -23,10 +24,135 @@ import lupos.s03resultRepresentation.ValueUndef
 import lupos.s03resultRepresentation.Variable
 import lupos.s04logicalOperators.noinput.OPNothing
 import lupos.s04logicalOperators.OPBase
+import lupos.s04logicalOperators.iterator.ColumnIterator
 import lupos.s04logicalOperators.OPBaseCompound
 import lupos.s04logicalOperators.Query
 
 object QueryResultToXMLStream {
+    suspend fun writeValue(valueID: Int, columnName: String,dictionary:ResultSetDictionary, output: PrintWriter) {
+        val value = dictionary.getValue(valueID)
+        when (value) {
+            is ValueBnode -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                    output.print("\">\n    <bnode>")
+                    output.print(valueID)
+                    output.print("</bnode>\n   </binding>\n")
+            }
+            is ValueBoolean -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal>")
+                output.print(value.value)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueLanguageTaggedLiteral -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal xml:lang=\"")
+                output.print(value.language)
+                output.print("\">")
+                output.print(value.content)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueSimpleLiteral -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal>")
+                output.print(value.content)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueTypedLiteral -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal datatype=\"")
+                output.print(value.type_iri)
+                output.print("\">")
+                output.print(value.content)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueDecimal -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#decimal>")
+                output.print(value.value)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueFloat -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#float>")
+                output.print(value.value)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueDouble -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#double>")
+                output.print(value.value)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueInteger -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#integer>")
+                output.print(value.value)
+                output.print("</literal>\n   </binding>\n")
+            }
+            is ValueIri -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n    <uri>")
+                output.print(value.iri)
+                output.print("</uri>\n   </binding>\n")
+            }
+            is ValueDateTime -> {
+                output.print("   <binding name=\"")
+                output.print(columnName)
+                output.print("\">\n")
+                if (value.timezoneHours == -1 && value.timezoneMinutes == -1) {
+                    output.print("<literal datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">${value.year.toString().padStart(4, '0')}-${value.month.toString().padStart(2, '0')}-${value.day.toString().padStart(2, '0')}T${value.hours.toString().padStart(2, '0')}:${value.minutes.toString().padStart(2, '0')}:${value.seconds.toString().padStart(2, '0')}</literal>\n")
+                } else if (value.timezoneHours == 0 && value.timezoneMinutes == 0) {
+                    output.print("<literal datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">${value.year.toString().padStart(4, '0')}-${value.month.toString().padStart(2, '0')}-${value.day.toString().padStart(2, '0')}T${value.hours.toString().padStart(2, '0')}:${value.minutes.toString().padStart(2, '0')}:${value.seconds.toString().padStart(2, '0')}Z</literal>\n")
+                } else {
+                    output.print("<literal datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">${value.year.toString().padStart(4, '0')}-${value.month.toString().padStart(2, '0')}-${value.day.toString().padStart(2, '0')}T${value.hours.toString().padStart(2, '0')}:${value.minutes.toString().padStart(2, '0')}:${value.seconds.toString().padStart(2, '0')}-${value.timezoneHours.toString().padStart(2, '0')}:${value.timezoneMinutes.toString().padStart(2, '0')}</literal>\n")
+                }
+                output.print("   </binding>\n")
+            }
+            is ValueUndef, is ValueError -> {
+            }
+        }
+    }
+
+    suspend fun writeRow(variables: Array<String>, rowBuf: IntArray,dictionary:ResultSetDictionary, output: PrintWriter) {
+        output.print("  <result>\n")
+        for (variableIndex in 0 until variables.size) {
+            writeValue(rowBuf[variableIndex], variables[variableIndex], dictionary,output)
+        }
+        output.print("  </result>\n")
+    }
+
+    suspend fun writeAllRows(variables: Array<String>, columns: Array<ColumnIterator>,dictionary:ResultSetDictionary, output: PrintWriter) {
+        val rowBuf = IntArray(variables.size)
+	val resultBuf=StringWriter()
+	val resultWriter=PrintWriter(resultBuf)
+        loop@ while (true) {
+            for (variableIndex in 0 until variables.size) {
+                val valueID = columns[variableIndex].next()
+                if (valueID == ResultSetDictionary.nullValue) {
+                    break@loop
+                }
+                rowBuf[variableIndex] = valueID
+            }
+            writeRow(variables, rowBuf, dictionary,resultWriter)
+		output.print(resultBuf.toString())
+		resultBuf.getBuffer().setLength(0)
+        }
+        for (closeIndex in 0 until columns.size) {
+            columns[closeIndex]!!.close()
+        }
+    }
+
     suspend operator fun invoke(rootNode: OPBase, output: PrintWriter) {
         val nodes: Array<OPBase>
         var columnProjectionOrder = listOf<List<String>>()
@@ -61,9 +187,7 @@ object QueryResultToXMLStream {
                 } else {
                     columnNames = node.getProvidedVariableNames()
                 }
-//var timer3 = DateHelper.markNow()
                 val child = node.evaluate(Partition())
-//println("timer #413 ${DateHelper.elapsedSeconds(timer3)}")
                 val variables = columnNames.toTypedArray()
                 if (variables.size == 1 && variables[0] == "?boolean") {
                     output.print(" <head/>\n")
@@ -75,7 +199,6 @@ object QueryResultToXMLStream {
                 } else {
                     val bnodeMap = MyMapIntInt()
                     var bnodeMapSize = 0
-                    val columns = variables.map { child.columns[it] }.toTypedArray()
                     if (variables.size == 0) {
                         output.print(" <head/>\n <results>\n")
                         for (j in 0 until child.count()) {
@@ -89,140 +212,9 @@ object QueryResultToXMLStream {
                             output.print(variable)
                             output.print("\">\n")
                         }
+                        val columns = variables.map { child.columns[it]!! }.toTypedArray()
                         output.print(" </head>\n <results>\n")
-//                        var timer = DateHelper.markNow()
-//                        var timer2 = DateHelper.markNow()
-//                        var timerNext = DateHelper.helperElapsed(timer, timer)
-//                        var timerPrint = timerNext
-//                        var timerFlush = timerNext
-                        loop@ while (true) {
-                            for (variableIndex in 0 until variables.size) {
-                                val valueID = columns[variableIndex]!!.next()
-                                if (valueID == ResultSetDictionary.nullValue) {
-                                    for (closeIndex in 0 until columns.size) {
-                                        columns[closeIndex]!!.close()
-                                    }
-                                    break@loop
-                                }
-//                                timer2 = DateHelper.markNow()
-//                                timerNext = DateHelper.helperAdd(timerNext, DateHelper.helperElapsed(timer, timer2))
-//                                timer = timer2
-                                if (variableIndex == 0) {
-                                    output.print("  <result>\n")
-                                }
-                                val value = node.query.dictionary.getValue(valueID)
-                                when (value) {
-                                    is ValueBnode -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        if (PRETTY_BNODE_NAMES) {
-                                            output.print("\">\n    <bnode>")
-                                            output.print(bnodeMap.getOrCreate(valueID, { bnodeMapSize++ }))
-                                            output.print("</bnode>\n   </binding>\n")
-                                        } else {
-                                            output.print("\">\n    <bnode>")
-                                            output.print(valueID)
-                                            output.print("</bnode>\n   </binding>\n")
-                                        }
-                                    }
-                                    is ValueBoolean -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal>")
-                                        output.print(value.value)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueLanguageTaggedLiteral -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal xml:lang=\"")
-                                        output.print(value.language)
-                                        output.print("\">")
-                                        output.print(value.content)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueSimpleLiteral -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal>")
-                                        output.print(value.content)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueTypedLiteral -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal datatype=\"")
-                                        output.print(value.type_iri)
-                                        output.print("\">")
-                                        output.print(value.content)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueDecimal -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#decimal>")
-                                        output.print(value.value)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueFloat -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#float>")
-                                        output.print(value.value)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueDouble -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#double>")
-                                        output.print(value.value)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueInteger -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <literal datatype=\"http://www.w3.org/2001/XMLSchema#integer>")
-                                        output.print(value.value)
-                                        output.print("</literal>\n   </binding>\n")
-                                    }
-                                    is ValueIri -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n    <uri>")
-                                        output.print(value.iri)
-                                        output.print("</uri>\n   </binding>\n")
-                                    }
-                                    is ValueDateTime -> {
-                                        output.print("   <binding name=\"")
-                                        output.print(variables[variableIndex])
-                                        output.print("\">\n")
-                                        if (value.timezoneHours == -1 && value.timezoneMinutes == -1) {
-                                            output.print("<literal datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">${value.year.toString().padStart(4, '0')}-${value.month.toString().padStart(2, '0')}-${value.day.toString().padStart(2, '0')}T${value.hours.toString().padStart(2, '0')}:${value.minutes.toString().padStart(2, '0')}:${value.seconds.toString().padStart(2, '0')}</literal>\n")
-                                        } else if (value.timezoneHours == 0 && value.timezoneMinutes == 0) {
-                                            output.print("<literal datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">${value.year.toString().padStart(4, '0')}-${value.month.toString().padStart(2, '0')}-${value.day.toString().padStart(2, '0')}T${value.hours.toString().padStart(2, '0')}:${value.minutes.toString().padStart(2, '0')}:${value.seconds.toString().padStart(2, '0')}Z</literal>\n")
-                                        } else {
-                                            output.print("<literal datatype=\"http://www.w3.org/2001/XMLSchema#dateTime\">${value.year.toString().padStart(4, '0')}-${value.month.toString().padStart(2, '0')}-${value.day.toString().padStart(2, '0')}T${value.hours.toString().padStart(2, '0')}:${value.minutes.toString().padStart(2, '0')}:${value.seconds.toString().padStart(2, '0')}-${value.timezoneHours.toString().padStart(2, '0')}:${value.timezoneMinutes.toString().padStart(2, '0')}</literal>\n")
-                                        }
-                                        output.print("   </binding>\n")
-                                    }
-                                    is ValueUndef, is ValueError -> {
-                                    }
-                                }
-                            }
-                            output.print("  </result>\n")
-//                            timer2 = DateHelper.markNow()
-//                            timerPrint = DateHelper.helperAdd(timerPrint, DateHelper.helperElapsed(timer, timer2))
-//                            timer = timer2
-/*
-                            output.flush()
-timer2=DateHelper.markNow()
-timerFlush=DateHelper.helperAdd(timerFlush,DateHelper.helperElapsed(timer,timer2))
-timer=timer2
-*/
-                        }
-//                        println("timer #410 ${DateHelper.helperToSeconds(timerNext)}")
-//                        println("timer #411 ${DateHelper.helperToSeconds(timerPrint)}")
-//                        println("timer #412 ${DateHelper.helperToSeconds(timerFlush)}")
+                        writeAllRows(variables, columns,node.query.dictionary, output)
                         output.print(" </results>\n")
                     }
                 }
