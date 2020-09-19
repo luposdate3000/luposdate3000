5a195d5aa33c495a31e89c6bbb18e3815a0eceea
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2020-09-19 08:59:33 +0000

    backup

--- src/commonMain/kotlin/lupos/s04logicalOperators/iterator/ColumnIteratorChildIterator.kt
+++ src/commonMain/kotlin/lupos/s04logicalOperators/iterator/ColumnIteratorChildIterator.kt
@@ -4,11 +4,43 @@ import lupos.s03resultRepresentation.ResultSetDictionary
 import lupos.s03resultRepresentation.Value
 
 abstract class ColumnIteratorChildIterator() : ColumnIterator() {
-    @JvmField
-    val childs = mutableListOf<ColumnIterator>()
+
+    var queue = Array<ColumnIterator?>(100) { null }
+    var queue_read = 0
+    var queue_write = 0
+
 
     @JvmField
     var label = 1
+
+    inline fun addChild(child: ColumnIterator) {
+        if ((1 + queue_write) % queue.size == queue_read) {
+            val buf = Array<ColumnIterator?>(queue.size * 2) { null }
+            var j = 0
+            if (queue_read < queue_write) {
+                for (i in queue_read until queue_write) {
+                    buf[j++] = queue[i]
+                }
+                queue_write = queue_write - queue_read
+            } else if (queue_read > queue_write) {
+                for (i in 0 until queue_write) {
+                    buf[j++] = queue[i]
+                }
+                for (i in queue_read until queue.size) {
+                    buf[j++] = queue[i]
+                }
+                queue_write = queue_write + queue.size - queue_read
+            } else {
+                queue_write = 0
+            }
+            queue_read = 0
+            queue = buf
+
+        }
+        queue[queue_write] = child
+        queue_write = (1 + queue_write) % queue.size
+    }
+
     inline fun closeOnNoMoreElements() {
         if (label != 0) {
             label = 2
@@ -17,35 +49,46 @@ abstract class ColumnIteratorChildIterator() : ColumnIterator() {
 
     inline suspend fun _close() {
         label = 0
-        while (childs.size > 0) {
-            childs.removeAt(0).close()
+        if (queue_read < queue_write) {
+            for (i in queue_read until queue_write) {
+                queue[i].close()
+            }
+        } else {
+            for (i in 0 until queue_write) {
+                queue[i].close()
+            }
+            for (i in queue_read until queue.size) {
+                queue[i].close()
+            }
         }
     }
 
-    inline suspend fun next_helper(crossinline onNoMoreElements: suspend () -> Unit,crossinline onClose: suspend () -> Unit): Value {
+    inline suspend fun next_helper(crossinline onNoMoreElements: suspend () -> Unit, crossinline onClose: suspend () -> Unit): Value {
         when (label) {
             1 -> {
                 while (true) {
-                    while (childs.size > 0) {
-                        val res = childs[0].next()
+                    while (queue_read != queue_write) {
+                        val res = queue[queue_read].next()
                         if (res == ResultSetDictionary.nullValue) {
-                            childs.removeAt(0).close()
+                            queue[queue_read].close()
+                            queue_read = (1 + queue_read) % queue.size
                         } else {
                             return res
                         }
                     }
                     onNoMoreElements()
-                    if (childs.size == 0) {
+                    if (queue_read == queue_write) {
                         onClose()
                         return ResultSetDictionary.nullValue
                     }
                 }
             }
             2 -> {
-                while (childs.size > 0) {
-                    val res = childs[0].next()
+                while (queue_read != queue_write) {
+                    val res = queue[queue_read].next()
                     if (res == ResultSetDictionary.nullValue) {
-                        childs.removeAt(0).close()
+                        queue[queue_read].close()
+                        queue_read = (1 + queue_read) % queue.size
                     } else {
                         return res
                     }
