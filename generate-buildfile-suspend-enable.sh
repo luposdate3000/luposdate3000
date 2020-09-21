18e95f4bd689b8103cc8c44aa0111203bb8cdc78
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2020-09-21 07:58:22 +0000

    a

--- generate-buildfile-suspend-enable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./generate-buildfile-suspend.kts
-find src.generated -name *.kt -print0 | xargs -0 -P 12 -n 20 ./generate-buildfile-suspend.kts Enable
---
+++ src-generate-buildfile/generate-buildfile-suspend-enable.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+$(echo $0 | sed "s-/[^/]*\$-/-g")generate-buildfile-suspend.kts
+find src.generated -name *.kt -print0 | xargs -0 -P 12 -n 20 $(echo $0 | sed "s-/[^/]*\$-/-g")generate-buildfile-suspend.kts Enable
