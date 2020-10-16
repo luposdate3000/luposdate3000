54722efa84e5bcfb8c4579aaacc3292e509d426b
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2020-10-16 15:51:19 +0000

    added content of accidently not committed folder

---
+++ src/luposdate3000_jena_wrapper_on/jsMain/kotlin/lupos/s00misc/JenaWrapper.kt
@@ -0,0 +1,23 @@
+package lupos.s00misc
+
+object JenaWrapper {
+    fun dropAll() {
+    }
+
+    fun updateQuery(queryString: String) {
+    }
+
+    fun execQuery(queryString: String): String {
+        var res = ""
+        return res
+    }
+
+    fun loadFromFile(fileName: String) {
+    }
+
+    fun loadFromFile(fileName: String, graph: String) {
+    }
+
+    fun execQueryFile(fileName: String) {
+    }
+}
