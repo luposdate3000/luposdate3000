41832a0dbf2326fe8958aa2e5bd4d7083f4804b7
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2020-11-19 15:07:53 +0000

    a

--- exec-endpoint.main.kts
+++ exec-endpoint.main.kts
@@ -52,3 +52,6 @@ ProcessBuilder(cmd)
         .redirectError(Redirect.INHERIT)
         .start()
         .waitFor()
+
+//curl -H "Content-Type: application/x-www-form-urlencoded" --data-binary "/mnt/luposdate-testdata/sp2b/1024/data0.n3" $(hostname):2324/import/turtle
+//curl -H "Content-Type: application/x-www-form-urlencoded" --data-binary "@resources/sp2b/q10.sparql" $(hostname):2324/sparql/query
