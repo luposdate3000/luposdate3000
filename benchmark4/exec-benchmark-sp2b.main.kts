dd83dc4e1d1fea75db0a6ae7e317f139cc053476
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2020-12-01 14:28:05 +0100

    a

---
+++ benchmark4/exec-benchmark-sp2b.main.kts
@@ -0,0 +1,88 @@
+#!/usr/bin/env kotlin
+@file:Import("../src/luposdate3000_shared/src/commonMain/kotlin/lupos/s00misc/EOperatingSystem.kt")
+@file:Import("../src/luposdate3000_shared_inline/src/commonMain/kotlin/lupos/s00misc/Platform.kt")
+@file:Import("../src/luposdate3000_shared_inline/src/jvmMain/kotlin/lupos/s00misc/Platform.kt")
+@file:Import("generate-data.main.kts")
+@file:CompilerOptions("-Xmulti-platform")
+
+import lupos.s00misc.Platform
+
+import java.io.File
+import java.lang.ProcessBuilder.Redirect
+import java.nio.file.StandardOpenOption
+import java.nio.file.Files
+import java.nio.file.Paths
+
+val triplesFiles = "${Platform.getBenchmarkHome()}/luposdate-testdata/bench_4/intermediate.n3"
+val minimumTime = 10.0
+
+File("log").mkdirs()
+val jars = mutableListOf(
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Buffer_Manager_Inmemory-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Dictionary_Inmemory-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Endpoint-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Operators-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Parser-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Result_Format-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Shared-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Test-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Triple_Store_All-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Triple_Store_Id_Triple-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Optimizer-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Endpoint_Java_Sockets-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Jena_Wrapper_Off-jvm.jar",
+        "build-cache${Platform.getPathSeparator()}bin${Platform.getPathSeparator()}Luposdate3000_Launch_Benchmark_4-jvm.jar",
+)
+val userHome = Platform.getUserHome()
+for (f in Platform.findNamedFileInDirectory("${Platform.getGradleCache()}modules-2${Platform.getPathSeparator()}files-2.1${Platform.getPathSeparator()}com.soywiz.korlibs.krypto${Platform.getPathSeparator()}krypto-jvm${Platform.getPathSeparator()}1.9.1${Platform.getPathSeparator()}", "krypto-jvm-1.9.1.jar")) {
+    jars.add(f)
+}
+for (f in Platform.findNamedFileInDirectory("${Platform.getMavenCache()}org${Platform.getPathSeparator()}jetbrains${Platform.getPathSeparator()}kotlin${Platform.getPathSeparator()}kotlin-stdlib${Platform.getPathSeparator()}1.4.255-SNAPSHOT", "kotlin-stdlib-1.4.255-SNAPSHOT.jar")) {
+    jars.add(f)
+}
+var classpath = ""
+for (jar in jars) {
+    if (classpath == "") {
+        classpath = jar
+    } else {
+        classpath = "$classpath:$jar"
+    }
+}
+
+
+for(output_count in listOf(32,128,512,2048,8196,32768,131072,524288,2097152)){
+for(trash in listOf(0,1,2,4,8,16,32,64,128,256,512,1024)){
+try{
+generateTriples("${Platform.getBenchmarkHome()}/luposdate-testdata/bench_4", output_count,trash,1): Int {
+val cmd = mutableListOf("java", "-Xmx${Platform.getAvailableRam()}g", "-cp", classpath, "MainKt", triplesFiles, "$minimumTime","$output_count","$trash","$join")
+cmd.addAll(args)
+ProcessBuilder(cmd)
+	.directory(File("."))
+        .redirectOutput(Redirect.INHERIT)
+        .redirectError(Redirect.INHERIT)
+        .start()
+        .waitFor()
+}catch(e:Exception){
+e.printStackTrace()
+}
+}
+for(join in listOf(1,2,4,8,16,32,64)){
+try{
+val count=output_count/(join*join)
+if(count==0){
+continue
+}
+generateTriples("${Platform.getBenchmarkHome()}/luposdate-testdata/bench_4", count,0,join): Int {
+val cmd = mutableListOf("java", "-Xmx${Platform.getAvailableRam()}g", "-cp", classpath, "MainKt", triplesFiles, "$minimumTime","$output_count", "$trash","$join")
+cmd.addAll(args)
+ProcessBuilder(cmd)
+        .directory(File("."))
+        .redirectOutput(Redirect.INHERIT)
+        .redirectError(Redirect.INHERIT)
+        .start()
+        .waitFor()
+}catch(e:Exception){ 
+e.printStackTrace()
+}
+}
+}
