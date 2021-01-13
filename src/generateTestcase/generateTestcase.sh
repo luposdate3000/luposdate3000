d655cf586fa23f2d4514017712a9ef08fd17982d
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-01-13 14:31:10 +0100

    a

--- src/generateTestcase/generateTestcase.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm generateTestcase.jar
-classpath="./build/build_1.4.255-SNAPSHOT_jvm_BinaryTestSuite_On_Threads_MultiMap_BPlusTreePartition_None_On_BTree_BTree_Empty_false_128_8_8_8_8_false_ECoverage.Disabled_Off_None_2000_Off_Off_true_EPOPDebugMode.DEBUG2/libs/luposdate3000-all.jar"
-/opt/kotlin/dist/kotlinc/bin/kotlinc generateTestcase.kt -cp $classpath -Xallow-jvm-ir-dependencies -d generateTestcase.jar -include-runtime
-java -cp "generateTestcase.jar:$classpath" GenerateTestcaseKt TestSuite . resources/binary
