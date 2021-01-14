65411ada2081be096877e37871bf38e92e0e19f6
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-01-14 12:21:33 +0100

    a

--- exec-benchmark-generate-sp2b.main.kts
+++ exec-benchmark-generate-sp2b.main.kts
@@ -1,8 +1,8 @@
 #!/usr/bin/env kotlin
 @file:Import("src/luposdate3000_shared/src/commonMain/kotlin/lupos/s00misc/EOperatingSystem.kt")
 @file:Import("src/luposdate3000_shared/src/commonMain/kotlin/lupos/s00misc/EOperatingSystemExt.kt")
-@file:Import("src/luposdate3000_shared_inline/src/commonMain/kotlin/lupos/s00misc/_Platform.kt")
-@file:Import("src/luposdate3000_shared_inline/src/jvmMain/kotlin/lupos/s00misc/_Platform.kt")
+@file:Import("src/luposdate3000_shared_inline/src/commonMain/kotlin/lupos/s00misc/Platform.kt")
+@file:Import("src/luposdate3000_shared_inline/src/jvmMain/kotlin/lupos/s00misc/Platform.kt")
 @file:Import("src/luposdate3000_scripting/exec-import.kt")
 @file:CompilerOptions("-Xmulti-platform")
 import lupos.s00misc.Platform
