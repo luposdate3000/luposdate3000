54cd2959f8c03aa210764690d10f85f49fa2b049
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-01-14 10:11:50 +0100

    a

--- tool-benchmark-operator-graph-to-pdf.sh
+++
@@ -1,7 +0,0 @@
-cd log
-for f in $(find -name "*sp2b*Distributed*.tex" | sed "s/-[0-9]*-/-/g" | sort | uniq)
-do
-	pdflatex $f
-done
-rm *.aux *.log combined.pdf
-pdfunite *.pdf combined.pdf
--- tool-operatorgraph-xml-to-tex.main.kts
+++ tool-operatorgraph-xml-to-tex.main.kts
@@ -1,5 +1,6 @@
 #!/usr/bin/env kotlin
 @file:Import("src/luposdate3000_shared/src/commonMain/kotlin/lupos/s00misc/OperatorGraphToLatex.kt")
+import lupos.s00misc.OperatorGraphToLatex
 val inputString = StringBuilder()
 while (true) {
     val line = readLine()
