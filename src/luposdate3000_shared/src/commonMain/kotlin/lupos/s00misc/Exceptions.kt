c45a1c89088937df0083a9f30b066bb6d22c7668
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-01-15 09:45:06 +0100

    add license to every file

--- src/luposdate3000_shared/src/commonMain/kotlin/lupos/s00misc/Exceptions.kt
+++ src/luposdate3000_shared/src/commonMain/kotlin/lupos/s00misc/Exceptions.kt
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Luposdate3000 distribution (https://github.com/luposdate3000/luposdate3000).
+ * Copyright (c) 2020-2021, Institute of Information Systems (Benjamin Warnke and contributors of LUPOSDATE3000), University of Luebeck
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package lupos.s00misc
 /* explicitly storing the classname has the advantage, that the classname is accessible in native code too, and not just via reflection */
 public abstract class Luposdate3000Exception public constructor(public val classname: String, msg: String) : Exception(msg)
