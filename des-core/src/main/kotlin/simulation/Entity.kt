d04a2b855748001a22a50e92270132b414518bf4
Johann <johann_mantler@web.de>
2021-01-21 23:43:56 +0100

    reimplement the busy function

--- des-core/src/main/kotlin/simulation/Entity.kt
+++ des-core/src/main/kotlin/simulation/Entity.kt
@@ -4,13 +4,13 @@ package simulation
 abstract class Entity {
 
     private val deferredEvents: EventPriorityQueue = EventPriorityQueue()
-    private var currentState = RUNNABLE
-    private var busyDuration = 0.0
+    private var currentState = State.RUNNABLE
+    private class EndBusyIdentifier
 
-    private companion object {
-        const val RUNNABLE = 1
-        const val BUSY = 2
-        const val TERMINATED = 2
+    private enum class State {
+        RUNNABLE,
+        BUSY,
+        TERMINATED,
     }
 
     abstract fun startUpEntity()
@@ -18,39 +18,42 @@ abstract class Entity {
     abstract fun shutDownEntity()
 
     fun addIncomingEvent(event: Event) {
+        if(isEndBusyIdentifier(event)) {
+            currentState = State.RUNNABLE
+        }
         deferredEvents.enqueue(event)
     }
 
+    private fun isEndBusyIdentifier(event: Event)
+        = event.data != null && event.data is EndBusyIdentifier
+
+
     fun processDeferredEvents() {
         var ev: Event
-        while (deferredEvents.hasNext() && currentState == RUNNABLE) {
+        while (deferredEvents.hasNext() && currentState == State.RUNNABLE) {
             ev = deferredEvents.dequeue()
             processEvent(ev)
         }
     }
 
     protected fun sendEvent(destination: Entity, delay: Double, type: Int, data: Any?) {
+        require(currentState == State.RUNNABLE)
         val event = Event(delay, this, destination, type, data)
         Simulation.sendEvent(event)
     }
 
-//    protected fun beBusy(duration: Double) {
-//        currentState = BUSY
-//        busyDuration = duration
-//        Simulation.setEntityBusy(this, duration)
-//
-//    }
 
-    private fun addBusyDuration(delay: Double)
-            = if(currentState == BUSY) delay + busyDuration else delay
 
-    fun stopBeingBusy() {
-        currentState = RUNNABLE
-        busyDuration = 0.0
+    protected fun beBusy(duration: Double) {
+        require(currentState == State.RUNNABLE)
+        currentState = State.BUSY
+        val event = Event(duration, this, this, 0, EndBusyIdentifier())
+        Simulation.sendEvent(event)
     }
 
+
     protected fun terminate() {
-        currentState = TERMINATED
+        currentState = State.TERMINATED
         deferredEvents.clear()
     }
 
