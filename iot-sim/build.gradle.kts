9c12f832edad0602000ad5ea9183c1292d7adc87
Johann <johann_mantler@web.de>
2021-02-02 21:02:59 +0100

    iot-sim has a dependency on des-core

--- iot-sim/build.gradle.kts
+++ iot-sim/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.javadocmd:simplelatlng:1.3.1")
+    implementation(project(":des-core"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
