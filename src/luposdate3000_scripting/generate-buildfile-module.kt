da31fefbea4b6dabe66e23202c023c382137fcf8
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-02-25 11:31:03 +0100

    compiling again with minified dependencies for jvm

--- src/luposdate3000_scripting/generate-buildfile-module.kt
+++ src/luposdate3000_scripting/generate-buildfile-module.kt
@@ -40,10 +40,10 @@ enum class IntellijMode {
     Enable, Disable
 }
 
-// var compilerVersion = "1.5.255-SNAPSHOT"
+var compilerVersion = "1.5.255-SNAPSHOT"
 
 // var compilerVersion = "1.4.255-SNAPSHOT"
-var compilerVersion = "1.4.0"
+// var compilerVersion = "1.4.0"
 val validPlatforms = listOf("iosArm32", "iosArm64", "linuxX64", "macosX64", "mingwX64")
 private fun printDependencies(dependencies: Set<String>, buildForIDE: Boolean, appendix: String, out: PrintWriter) {
     for (d in dependencies) {
