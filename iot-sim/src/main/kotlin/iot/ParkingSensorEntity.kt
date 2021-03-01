09e79589e6d1f7f5e82501ef09ed7f3516e64973
Johann <johann_mantler@web.de>
2021-03-01 15:19:40 +0100

    remove unnecessary null checks

--- iot-sim/src/main/kotlin/iot/ParkingSensorEntity.kt
+++ iot-sim/src/main/kotlin/iot/ParkingSensorEntity.kt
@@ -14,7 +14,7 @@ class ParkingSensorEntity(
     override fun processEvent(event: Event) {
         if(isBusyEndEvent(event)) {
             sendObservationToSink()
-            if(device!!.powerSupply.hasPowerLeft())
+            if(device.powerSupply.hasPowerLeft())
                 observeEnvironment()
             else
                 terminate()
@@ -23,20 +23,20 @@ class ParkingSensorEntity(
 
     private fun sendObservationToSink() {
         val dataPackage = createNetworkPackage(createObservationData())
-        sendEvent(device!!.networkCard, 0, dataPackage)
-        device!!.powerSupply.decrease()
+        sendEvent(device.networkCard, 0, dataPackage)
+        device.powerSupply.decrease()
     }
 
     private fun observeEnvironment() {
         beBusy(dataRateInSeconds.toLong())
-        device!!.powerSupply.decrease()
+        device.powerSupply.decrease()
     }
 
     private fun createObservationData()
         = ParkingObservation(RandomGenerator.random.nextBoolean())
 
     private fun createNetworkPackage(data: ParkingObservation)
-        = NetworkPackage(device!!, dataSink!!, data)
+        = NetworkPackage(device, dataSink, data)
 
     override fun shutDownEntity() {
         terminate()
