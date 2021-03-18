6ef72098cfd9e72c6a3531cc78e7aa94a48a5895
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-03-18 09:29:43 +0100

    a

--- src/luposdate3000_launch_test_buffermanager/src/commonMain/kotlin/lupos/launch/import/MainFunc.kt
+++ src/luposdate3000_launch_test_buffermanager/src/commonMain/kotlin/lupos/launch/import/MainFunc.kt
@@ -18,136 +18,223 @@ package lupos.launch.import
 
 import lupos.buffermanager.BufferManager
 import lupos.s00misc.ByteArrayHelper
-import lupos.s00misc.File
+import lupos.s00misc.Parallel
 
 @OptIn(ExperimentalStdlibApi::class, kotlin.time.ExperimentalTime::class)
-internal fun mainFunc(filename: String): Unit = Parallel.runBlocking {
-    val bufferManager = BufferManager("")
+internal fun mainFunc(seedString: String): Unit = Parallel.runBlocking {
+    val seed = seedString.toInt()
+    val random = Random(seed)
+    while (true) {
+        val bufferManager = BufferManager("")
 
-    val pageIds = mutableListOf<Int>()
-    val mappedPages = mutableMapOf<Int, ByteArray>()
-    val mappedPagesCtr = mutableMapOf<Int, Int>()
+        val pageIds = mutableListOf<Int>()
+        val mappedPages = mutableMapOf<Int, ByteArray>()
+        val mappedPagesCtr = mutableMapOf<Int, Int>()
 
-    val f = File(filename).withInputStream { input ->
-        while (true) { // TODO input available
-// TODO check all mapped pages
-            val mode = input.readInt()
+        fun testReleasePageOk(pageid: Int) {
+            bufferManager.releasePage(pageid)
+            val cnt = mappedPagesCtr[pageid]
+            if (cnt == null) {
+                throw Exception("")
+            }
+            if (cnt == 1) {
+                mappedPages.remove(pageid)
+                mappedPagesCtr.remove(pageid)
+            } else {
+                mappedPagesCtr[pageid] = cnt - 1
+            }
+        }
+
+        fun testReleasePageFail(pageid: Int) {
+            var flag = true
+            try {
+                bufferManager.releasePage(pageid)
+            } catch (e: Throwable) {
+                flag = false
+            }
+            if (flag) {
+                throw Exception("")
+            }
+        }
+
+        fun testGetPageOk(pageid: Int) {
+            val page = bufferManager.getPage(pageid)
+            val id = ByteArrayHelper.readInt4(page, 0)
+            if (id != pageid) {
+                throw Exception("")
+            }
+            val pp = mappedPages[pageid]
+            mappedPages[pageid] = page
+            if (pp != null) {
+                val cnt = mappedPagesCtr[pageid]!!
+                mappedPagesCtr[pageid] = cnt + 1
+                if (pp != page) {
+                    throw Exception("")
+                }
+            } else {
+                mappedPagesCtr[pageid] = 1
+            }
+        }
+
+        fun testGetPageFail(pageid: Int) {
+            var flag = true
+            try {
+                bufferManager.getPage(pageid)
+            } catch (e: Throwable) {
+                flag = false
+            }
+            if (flag) {
+                throw Exception("")
+            }
+        }
+
+        fun testCreateNewPageOk() {
+            bufferManager.createPage { page, pageid ->
+                ByteArrayHelper.writeInt4(page, 0, pageid)
+                if (pageIds.contains(pageid)) {
+                    throw Exception("")
+                }
+                pageIds.add(pageid)
+                if (mappedPages[pageid] != null) {
+                    throw Exception("")
+                }
+                mappedPages[pageid] = page
+                if (mappedPagesCtr[pageid] != null) {
+                    throw Exception("")
+                }
+                mappedPagesCtr[pageid] = 1
+            }
+        }
+
+        fun testDeletePageOk(pageid: Int) {
+            bufferManager.deletePage(pageid)
+            mappedPagesCtr.remove(pageid)
+            mappedPages.remove(pageid)
+            pageIds.remove(pageid)
+        }
+
+        fun testDeletePageFail(pageid: Int) {
+            var flag = true
+            try {
+                bufferManager.deletePage(pageid)
+            } catch (e: Throwable) {
+                flag = false
+            }
+            if (flag) {
+                throw Exception("")
+            }
+        }
+
+        fun getPageID_Existing_Mapped(rnd: Int, action: (Int) -> Unit) {
+            val ids = mutableListOf<Int>()
+            ids.addAll(mappedPages.keys)
+            if (ids.size > 0) {
+                val pageid = ids[rnd % ids.size]
+                action(pageid)
+            }
+        }
+
+        fun getPageID_Existing_NotMapped(rnd: Int, action: (Int) -> Unit) {
+            val ids = mutableListOf<Int>()
+            ids.addAll(pageIds)
+            ids.removeAll(mappedPages.keys)
+            if (ids.size > 0) {
+                val pageid = ids[rnd % ids.size]
+                action(pageid)
+            }
+        }
+
+        fun getPageID_NotExisting(rnd: Int, action: (Int) -> Unit) {
+            val ids = MutableList<Int>(1000) { it }
+            ids.removeAll(pageIds)
+            if (ids.size > 0) {
+                val pageid = ids[rnd % ids.size]
+                action(pageid)
+            }
+        }
+
+        fun getPageID_Existing(rnd: Int, action: (Int) -> Unit) {
+            if (pageIds.size > 0) {
+                val pageid = pageIds[rnd % pageIds.size]
+                action(pageid)
+            }
+        }
+
+        fun getPageID_Existing_SingleMapped(rnd: Int, action: (Int) -> Unit) {
+            val ids = mutableListOf<Int>()
+            for ((k, v) in mappedPagesCtr) {
+                if (v == 1) {
+                    ids.add(k)
+                }
+            }
+            if (ids.size > 0) {
+                val pageid = ids[rnd % ids.size]
+                action(pageid)
+            }
+        }
+
+        fun getPageID_Existing_MultiMapped(rnd: Int, action: (Int) -> Unit) {
+            val ids = mutableListOf<Int>()
+            for ((k, v) in mappedPagesCtr) {
+                if (v > 1) {
+                    ids.add(k)
+                }
+            }
+            if (ids.size > 0) {
+                val pageid = ids[rnd % ids.size]
+                action(pageid)
+            }
+        }
+        testCreateNewPageOk()
+        val cnt = random.nextInt(0, 65536)
+        for (i in 0 until cnt) {
+            var mode = random.nextInt(0, 10)
+            var rnd = random.nextInt()
             when (mode) {
-                0 -> { // release existing mapped page
-                    val ids = mutableListOf<Int>()
-                    ids.addAll(mappedPages.keys)
-                    if (ids.size > 0) {
-                        val pageid = ids[input.readInt() % ids.size]
-                        bufferManager.releasePage(pageid)
-                        val cnt = mappedPagesCtr[pageid]
-                        if (cnt == null) {
-                            throw Excepton("")
-                        }
-                        if (cnt == 1) {
-                            mappedPages.remove(pageid)
-                            mappedPagesCtr.remove(pageid)
-                        } else {
-                            mappedPagesCtr[pageid] = cnt - 1
-                        }
-                    }
-                }
-                4 -> { // release existing not mapped page
-                    val ids = mutableListOf<Int>()
-                    ids.addAll(pageIds)
-                    ids.removeAll(mappedPages.keys)
-                    if (ids.size > 0) {
-                        val pageid = ids[input.readInt() % ids.size]
-                        var flag = true
-                        try {
-                            bufferManager.releasePage(pageid)
-                        } catch (e: Throwable) {
-                            flag = false
-                        }
-                        if (flag) {
-                            throw Exception("")
-                        }
-                    }
-                }
-                5 -> { // release not existing page
-                    val ids = MutableList<Int>(1000) { it }
-                    ids.removeAll(pageIds)
-                    if (ids.size > 0) {
-                        val pageid = ids[input.readInt() % ids.size]
-                        var flag = true
-                        try {
-                            bufferManager.releasePage(pageid)
-                        } catch (e: Throwable) {
-                            flag = false
-                        }
-                        if (flag) {
-                            throw Exception("")
-                        }
-                    }
-                }
-                1 -> { // get existing page
-                    if (pageIds.size > 0) {
-                        val pageid = pageIds[input.readInt() % pageIds.size]
-                        val page = bufferManager.getPage(pageid)
-                        val id = ByteArrayHelper.readInt4(page, 0)
-                        if (id != pageid) {
-                            throw Exception("")
-                        }
-                        val pp = mappedPages[pageid]
-                        mappedPages[pageid] = page
-                        if (pp != null) {
-                            val cnt = mappedPagesCtr[pageid]!!
-                            mappedPagesCtr[pageid] = cnt + 1
-                            if (pp != page) {
-                                throw Excepton("")
-                            }
-                        } else {
-                            mappedPagesCtr[pageid] = 1
-                        }
-                    }
-                }
-                6 -> { // get not existing page
-                }
-                2 -> { // create new page
-                    bufferManager.createPage { page, pageid ->
-                        ByteArrayHelper.writeInt4(page, 0, pageid)
-                        if (pageIds.contains(pageid)) {
-                            throw Excepton("")
-                        }
-                        pageIds.add(pageid)
-                        if (mappedPages[pageid] != null) {
-                            throw Excepton("")
-                        }
-                        mappedPages[pageid] = page
-                        if (mappedPagesCtr[pageid] != null) {
-                            throw Excepton("")
-                        }
-                        mappedPagesCtr[pageid] = 1
-                    }
-                }
-                3 -> { // delete existing singlemapped page
-                    val ids = mutableSetOf<Int>()
-                    for ((k, v) in mappedPagesCtr) {
-                        if (v == 1) {
-                            ids.add(k)
-                        }
-                    }
-                    if (ids.size > 0) {
-                        val pageid = ids[input.readInt() % ids.size]
-                        bufferManager.deletePage(pageid)
-                        mappedPagesCtr.remove(pageid)
-                        mappedPages.remove(pageid)
-                        pageIds.remove(pageid)
-                    }
-                }
-                7 -> { // delete existing not mapped page
-                }
-                8 -> { // delete existing multiple mapped page
-                }
-                9 -> { // delete not existing page
+                0 -> getPageID_Existing_Mapped(rnd) { testReleasePageOk(it) }
+                1 -> getPageID_Existing_NotMapped(rnd) { testReleasePageFail(it) }
+                2 -> getPageID_NotExisting(rnd) { testReleasePageFail(it) }
+
+                3 -> getPageID_Existing(rnd) { testGetPageOk(it) }
+                4 -> getPageID_NotExisting(rnd) { testGetPageFail(it) }
+
+                5 -> testCreateNewPageOk()
+
+                6 -> getPageID_Existing_SingleMapped(rnd) { testDeletePageOk(it) }
+                7 -> getPageID_Existing_NotMapped(rnd) { testDeletePageFail(it) }
+                8 -> getPageID_Existing_MultiMapped(rnd) { testDeletePageFail(it) }
+                9 -> getPageID_NotExisting(rnd) { testDeletePageFail(it) }
+            }
+            for ((k, v) in mappedPages) {
+                val id = ByteArrayHelper.readInt4(v, 0)
+                if (id != k) {
+                    throw Exception("")
                 }
             }
         }
-// TODO unmap all
-// TODO check all pages
+        var ids = mutableMapOf<Int, Int>()
+        ids.putAll(mappedPagesCtr)
+        for ((k, v) in ids) {
+            for (i in 0 until v) {
+                testReleasePageOk(v)
+            }
+        }
+        for (pageid in pageIds) {
+            val page = bufferManager.getPage(pageid)
+            val id = ByteArrayHelper.readInt4(page, 0)
+            if (id != pageid) {
+                throw Exception("")
+            }
+            bufferManager.deletePage(pageid)
+        }
+        if (pageIds.size > 0) {
+            throw Exception("")
+        }
+        if (mappedPages.size > 0) {
+            throw Exception("")
+        }
+        if (mappedPagesCtr.size > 0) {
+            throw Exception("")
+        }
     }
 }
