a2259dfabdd2df35e6233655466f077f73a2af1a
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-03-30 12:34:04 +0200

    a

--- src/luposdate3000_launch_test_dictionary_encoding/src/commonMain/kotlin/lupos/launch/test_dictionary_encoding/MainFunc.kt
+++ src/luposdate3000_launch_test_dictionary_encoding/src/commonMain/kotlin/lupos/launch/test_dictionary_encoding/MainFunc.kt
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package lupos.launch.test_dictionary
+package lupos.launch.test_dictionary_encoding
 
 import lupos.buffermanager.BufferManagerExt
 import lupos.dictionary.DictionaryHelper
