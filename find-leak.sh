8a9a205039019fa704e9d00b57eb7000fac13f15
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-04-08 13:44:28 +0200

    a

--- find-leak.sh
+++ find-leak.sh
@@ -1,19 +1,9 @@
-#!/bin/bash
-#for buffermanager in Inmemory Persistent
-for buffermanager in Inmemory
-do
-#for endpoint_launcher in Java_Sockets None
-for endpoint_launcher in Java_Sockets
-do
-#for jena in On Off
-for jena in Off
-do
 cat <<EOT > config.pro
--injars ./Luposdate3000_Buffer_Manager_${buffermanager}-jvm.jar
+-injars ./Luposdate3000_Buffer_Manager_Inmemory-jvm.jar
 -injars ./Luposdate3000_Dictionary-jvm.jar
--injars ./Luposdate3000_Endpoint_${endpoint_launcher}-jvm.jar
+-injars ./Luposdate3000_Endpoint_Java_Sockets-jvm.jar
 -injars ./Luposdate3000_Endpoint-jvm.jar
--injars ./Luposdate3000_Jena_Wrapper_${jena}-jvm.jar
+-injars ./Luposdate3000_Jena_Wrapper_Off-jvm.jar
 -injars ./Luposdate3000_Operators-jvm.jar
 -injars ./Luposdate3000_Optimizer_Ast-jvm.jar
 -injars ./Luposdate3000_Optimizer_Logical-jvm.jar
@@ -32,7 +22,7 @@ cat <<EOT > config.pro
 -injars ./Luposdate3000_Launch_Binary_Test_Suite-jvm.jar
 -injars ./Luposdate3000_Launch_Generate_Binary_Test_Suite_Single-jvm.jar
 -injars ./Luposdate3000_Launch_Generate_Binary_Test_Suite_Multi-jvm.jar
--outjars XXX_${buffermanager}_${endpoint_launcher}_${jena}.jar
+-outjars XXX_Inmemory_Java_Sockets_Off.jar
 EOT
 cat *classpath | sort | uniq | sed "s/^/-libraryjars /g" >> config.pro
 echo '-libraryjars /usr/lib/jvm/java-1.11.0-openjdk-amd64/jmods/java.base.jmod(!**.jar;!module-info.class)' >>config.pro
@@ -41,9 +31,9 @@ cat <<EOT >> config.pro
 -optimizationpasses 5
 -allowaccessmodification
 -dontobfuscate
--printmapping XXX_${buffermanager}_${endpoint_launcher}_${jena}.mapping.txt
--printconfiguration XXX_${buffermanager}_${endpoint_launcher}_${jena}.config.pro.generated
--printusage XXX_${buffermanager}_${endpoint_launcher}_${jena}.usage.txt
+-printmapping XXX_Inmemory_Java_Sockets_Off.mapping.txt
+-printconfiguration XXX_Inmemory_Java_Sockets_Off.config.pro.generated
+-printusage XXX_Inmemory_Java_Sockets_Off.usage.txt
 
 -keep public class lupos.launch.** {
     *;
@@ -55,7 +45,4 @@ cat <<EOT >> config.pro
 
 -ignorewarnings
 EOT
-/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -jar /usr/share/java/proguard.jar @config.pro > XXX_${buffermanager}_${endpoint_launcher}_${jena}.log 2>&1
-done
-done
-done
+/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -jar /usr/share/java/proguard.jar @config.pro > XXX_Inmemory_Java_Sockets_Off.log 2>&1
