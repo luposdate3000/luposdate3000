0a2f5d18eff46228871c178b664237608e3426eb
Christin Krause <christin.krause@student.uni-luebeck.de>
2021-04-08 09:38:09 +0200

    Additon of while loop for time to BenchmarkClass

--- src/luposdate3000_launch_code_gen_example_kapt/src/commonMain/kotlin/examplePackage/BenchmarkClass.kt
+++ src/luposdate3000_launch_code_gen_example_kapt/src/commonMain/kotlin/examplePackage/BenchmarkClass.kt
@@ -5,43 +5,44 @@ import lupos.endpoint.LuposdateEndpoint
 import lupos.s00misc.CodeGenerationAnnotation
 import lupos.s00misc.DateHelperRelative
 
-
-
-
 @OptIn(ExperimentalStdlibApi::class, kotlin.time.ExperimentalTime::class)
 public class BenchmarkClass {
 
     init {
         LuposdateEndpoint.importTurtleFiles(
-            "resources/code-generation/example.n3",
+            "resources/code-generation/example10000.n3",
             mutableMapOf()
         )
+        println("Init finished")
     }
+
     @JvmField
     @CodeGenerationAnnotation
-    public val exampleVar: String = "SELECT ?article ?pages (?pages <= 20 as ?x) WHERE {?article <http://swrc.ontoware.org/ontology#pages> ?pages . FILTER(?pages <= 50)}"
+    public val exampleVar: String = "SELECT ?pages ?article ?title  WHERE {?article <http://swrc.ontoware.org/ontology#pages> ?pages . ?article <http://purl.org/dc/elements/1.1/title> ?title}"
 
-
-    internal fun startTimer() : Double {
+    internal fun startTimer() : Pair<Double,Int> {
         var time: Double = 0.0
-        for (count in 1..1000){
-            val timer = DateHelperRelative.markNow()
-exampleVar_evaluate()
-            time += DateHelperRelative.elapsedSeconds(timer)
-    }
-        return time/1000
-    }
-
-    internal fun startTimerEndpoint() : Double {
-        var time: Double = 0.0
-        for (count in 1..1000){
-            val timer = DateHelperRelative.markNow()
-            LuposdateEndpoint.evaluateSparqlToResultB(exampleVar)
-            time += DateHelperRelative.elapsedSeconds(timer)
+        var counter: Int = 0
+        exampleVar_evaluate()
+        val timer = DateHelperRelative.markNow()
+        while (time < 10.0){
+            exampleVar_evaluate()
+            time = DateHelperRelative.elapsedSeconds(timer)
+            counter ++
         }
-        return time/1000
+        return Pair(time/counter,counter)
     }
 
-
-
+    internal fun startTimerEndpoint() : Pair<Double,Int> {
+        var time: Double = 0.0
+        var counter: Int = 0
+        LuposdateEndpoint.evaluateSparqlToResultB(exampleVar)
+        val timer = DateHelperRelative.markNow()
+        while (time < 10.0){
+            LuposdateEndpoint.evaluateSparqlToResultB(exampleVar)
+            time = DateHelperRelative.elapsedSeconds(timer)
+            counter ++
+        }
+        return Pair(time/counter,counter)
+    }
 }
