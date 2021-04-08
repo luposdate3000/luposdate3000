6d919dc9b5e38d05e58b8c4d7f968c2dd60a80ef
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-04-08 14:02:43 +0200

    a

--- find-leak.sh
+++ find-leak.sh
@@ -1,48 +1,26 @@
-cat <<EOT > config.pro
--injars ./Luposdate3000_Buffer_Manager_Inmemory-jvm.jar
--injars ./Luposdate3000_Dictionary-jvm.jar
--injars ./Luposdate3000_Endpoint_Java_Sockets-jvm.jar
--injars ./Luposdate3000_Endpoint-jvm.jar
--injars ./Luposdate3000_Jena_Wrapper_Off-jvm.jar
--injars ./Luposdate3000_Operators-jvm.jar
--injars ./Luposdate3000_Optimizer_Ast-jvm.jar
--injars ./Luposdate3000_Optimizer_Logical-jvm.jar
--injars ./Luposdate3000_Optimizer_Physical-jvm.jar
--injars ./Luposdate3000_Parser-jvm.jar
--injars ./Luposdate3000_Result_Format-jvm.jar
--injars ./Luposdate3000_Shared-jvm.jar
--injars ./Luposdate3000_Test-jvm.jar
--injars ./Luposdate3000_Triple_Store_Manager-jvm.jar
--injars ./Luposdate3000_Triple_Store_Id_Triple-jvm.jar
--injars ./Luposdate3000_Launch_Endpoint-jvm.jar
--injars ./Luposdate3000_Launch_Benchmark-jvm.jar
--injars ./Luposdate3000_Launch_Sparql_Test_Suite-jvm.jar
--injars ./Luposdate3000_Launch_Import-jvm.jar
--injars ./Luposdate3000_Launch_Code_Gen_Example_KAPT-jvm.jar
--injars ./Luposdate3000_Launch_Binary_Test_Suite-jvm.jar
--injars ./Luposdate3000_Launch_Generate_Binary_Test_Suite_Single-jvm.jar
--injars ./Luposdate3000_Launch_Generate_Binary_Test_Suite_Multi-jvm.jar
--outjars XXX_Inmemory_Java_Sockets_Off.jar
-EOT
-cat *classpath | sort | uniq | sed "s/^/-libraryjars /g" >> config.pro
+#./launcher.main.kts --run --mainClass=Endpoint --endpointMode=Java_Sockets --inlineMode=Enable --releaseMode=Enable --dryMode=Enable
+rm config.pro
+for f in $(./launcher.main.kts --run --mainClass=Endpoint --endpointMode=Java_Sockets --inlineMode=Enable --releaseMode=Enable --dryMode=Enable | tr ":" "\n" | tr " " "\n" | sort | grep "^build-cache.*\.jar$")
+do
+echo "-injars ./$f" >> config.pro
+done
+for f in $(./launcher.main.kts --run --mainClass=Endpoint --endpointMode=Java_Sockets --inlineMode=Enable --releaseMode=Enable --dryMode=Enable | tr ":" "\n" | tr " " "\n" | sort | grep "^/root.*\.jar$")
+do
+echo "-libraryjars $f" >> config.pro
+done
+echo "-outjars find-leak.jar" >> config.pro
 echo '-libraryjars /usr/lib/jvm/java-1.11.0-openjdk-amd64/jmods/java.base.jmod(!**.jar;!module-info.class)' >>config.pro
 cat <<EOT >> config.pro
 -forceprocessing
 -optimizationpasses 5
 -allowaccessmodification
 -dontobfuscate
--printmapping XXX_Inmemory_Java_Sockets_Off.mapping.txt
--printconfiguration XXX_Inmemory_Java_Sockets_Off.config.pro.generated
--printusage XXX_Inmemory_Java_Sockets_Off.usage.txt
-
--keep public class lupos.launch.** {
-    *;
+-printmapping find-leak.mapping.txt
+-printconfiguration find-leak.config.pro.generated
+-printusage find-leak.usage.txt
+-keep public class lupos.endpoint.LuposdateEndpoint{
+    public String importIntermediateFiles(String);
 }
-
--keep public class lupos.s16network.LuposdateEndpoint{
-    *;
-}
-
 -ignorewarnings
 EOT
-/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -jar /usr/share/java/proguard.jar @config.pro > XXX_Inmemory_Java_Sockets_Off.log 2>&1
+/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -jar /usr/share/java/proguard.jar @config.pro > find-leak.log 2>&1
