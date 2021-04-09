4699e18933cbcdae0718bb208f45863160e8d4e2
Johann <johann_mantler@web.de>
2021-04-09 14:10:12 +0200

    refactor: remove dead code

--- iot-sim/src/main/kotlin/ConnectionParameter.kt
+++
@@ -1,7 +0,0 @@
-//TODO Ersetze durch Link
-data class ConnectionParameter(
-    val dataRateInKbps: Int,
-    val maxDistanceInMeters: Double,
-    val protocolName: String,
-    var distanceInMeters: Double,
-)
