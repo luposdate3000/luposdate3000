5cf7ea6a6293efdfeaac9fcfa89a049da6f7d0c2
Johann <johann_mantler@web.de>
2021-05-01 20:00:38 +0200

    refactor: rename

--- iot-sim/src/test/kotlin/SimulationIntegrationTest.kt
+++ iot-sim/src/test/kotlin/SimulationIntegrationTest.kt
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class SimulationTest {
+class SimulationIntegrationTest {
 
 
     @ParameterizedTest
