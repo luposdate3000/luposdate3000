bfee35003c080b234674891e624b4252d51a04e1
Johann <johann_mantler@web.de>
2021-05-31 13:06:40 +0200

    refactor

--- iot-sim/src/main/kotlin/IRoutingAlgorithm.kt
+++ iot-sim/src/main/kotlin/IRoutingAlgorithm.kt
@@ -1,7 +1,6 @@
 interface IRoutingAlgorithm {
     fun startRouting()
     fun isControlPackage(pck: NetworkPackage): Boolean
-
     fun processControlPackage(pck: NetworkPackage)
     fun getNextHop(destinationAddress: Int): Int
     fun getNextDatabaseHop(destinationAddress: Int): Int
