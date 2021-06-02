c98fd656082cfbeb1c79e4b7af655e65abcef2f1
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-06-02 13:48:05 +0000

    backup

--- src/luposdate3000_launch_generate_arithmetik_operators/src/commonMain/kotlin/lupos/launch/generate_arithmetik_operators/MainFunc.kt
+++ src/luposdate3000_launch_generate_arithmetik_operators/src/commonMain/kotlin/lupos/launch/generate_arithmetik_operators/MainFunc.kt
@@ -14,20 +14,20 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package lupos.launch.lupos.launch.osdate3000_launh_generate_arithmetik_operators
+package lupos.launch.generate_arithmetik_operators
 
 import lupos.code_generator_shared.MyOperator
 import lupos.code_generator_shared.OperatorBuilder
 import lupos.shared.Parallel
-import java.io.File
+import lupos.shared_inline.File
 
 @OptIn(ExperimentalStdlibApi::class, kotlin.time.ExperimentalTime::class)
 internal fun mainFunc(): Unit = Parallel.runBlocking {
-    public val operators = mutableListOf<MyOperator>()
+    val operators = mutableListOf<MyOperator>()
 
     OperatorBuilder.build(operators)
     for (operator in operators) {
-        File("src/luposdate3000_operator_arithmetik/src/commonMain/kotlin/lupos/operator/arithmetik/generated/AOP${operator.type.str}${operator.name}.kt").printWriter().use { out ->
+        File("src/luposdate3000_operator_arithmetik/src/commonMain/kotlin/lupos/operator/arithmetik/generated/AOP${operator.type.str}${operator.name}.kt").withOutputStream { out ->
             out.println("/*")
             out.println(" * This file is part of the Luposdate3000 distribution (https://github.com/luposdate3000/luposdate3000).")
             out.println(" * Copyright (c) 2020-2021, Institute of Information Systems (Benjamin Warnke and contributors of LUPOSDATE3000), University of Luebeck")
--- src/luposdate3000_launch_generate_arithmetik_operators/src/jsMain/kotlin/Main.kt
+++ src/luposdate3000_launch_generate_arithmetik_operators/src/jsMain/kotlin/Main.kt
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-import lupos.launch.osdate3000_launh_generate_arithmetik_operators.mainFunc
+import lupos.launch.generate_arithmetik_operators.mainFunc
 import kotlin.js.JsName
 
 @JsName("main")
