344d340fa6f3b29b9651ef079baa3ba8ddac8542
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-06-09 06:40:59 +0000

    remove unnecessary files

--- simulation/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    val kotlinVersion = "1.4.21"
-    kotlin("jvm") version kotlinVersion apply false
-    kotlin("plugin.serialization") version kotlinVersion apply false
-}
-
--- simulation/settings.gradle.kts
+++
@@ -1,4 +0,0 @@
-rootProject.name = "IoTSimulator"
-include("des-core")
-include("iot-sim")
-include("iot-db-interface")
