9ffd8fbecbaa95bdd948d70d9cb00c336df8257e
Johann <johann_mantler@web.de>
2021-06-14 19:31:16 +0200

    Add time measurements for the total simulation time

--- src/luposdate3000_simulator_iot/src/jvmMain/kotlin/IoTSimulation.kt
+++ src/luposdate3000_simulator_iot/src/jvmMain/kotlin/IoTSimulation.kt
@@ -4,32 +4,10 @@ import kotlinx.datetime.*
 import lupos.simulator_core.Simulation
 import lupos.simulator_iot.config.Configuration
 
-public fun main() {
-}
 
 public class IoTSimulation {
 
-    /*
-    * Lasse erstmal die Simulation laufen wie im
-    * test, und schaue was dabei herauskommt.
-    *
-    * Idee: 3 Configurationen mit verschiedenen Datensenken
-    *
-    *
-    * */
-    public fun run() {
-        val configFileName = ""
-        Configuration.parse(configFileName)
-        val sim = Simulation(Configuration.devices, callback = Logger())
 
-        sim.startSimulation()
-    }
 
-    public fun getDateTimeString(simClock: Long) {
-        val now: Instant = Clock.System.now()
-        val timeZone = TimeZone.currentSystemDefault()
-        val simTime = now.plus(2, DateTimeUnit.MILLISECOND, timeZone)
 
-        val time = simTime.toString()
-    }
 }
