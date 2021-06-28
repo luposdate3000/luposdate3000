1b5ce58755e03144d25d2105f56b6f5381db1766
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-06-28 14:30:50 +0000

    adding working tests such that they dont break

---
+++ src/luposdate3000_launch_code_gen_test/src/jvmTest/kotlin/lupos/launch/code_gen_test/common/SimpleDELETEWHERE3.kt
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the Luposdate3000 distribution (https://github.com/luposdate3000/luposdate3000).
+ * Copyright (c) 2020-2021, Institute of Information Systems (Benjamin Warnke and contributors of LUPOSDATE3000), University of Luebeck
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package lupos.launch.code_gen_test
+import lupos.endpoint.LuposdateEndpoint
+import lupos.operator.arithmetik.noinput.AOPVariable
+import lupos.result_format.EQueryResultToStreamExt
+import lupos.shared.EIndexPatternExt
+import lupos.shared.MemoryTable
+import lupos.shared.inline.File
+import lupos.shared.inline.MyPrintWriter
+import kotlin.test.Test
+import kotlin.test.fail
+
+public class SimpleDELETEWHERE3 {
+    val inputData = File("src/jvmTest/resources/SimpleDELETEWHERE3.input").readAsString()
+    val targetData = File("src/jvmTest/resources/SimpleDELETEWHERE3.output").readAsString()
+    val targetType = ".ttl"
+    val inputType = ".ttl"
+    val query = File("src/jvmTest/resources/SimpleDELETEWHERE3.query").readAsString()
+
+    @Test fun `Simple DELETE WHERE 3}`() {
+        val instance = LuposdateEndpoint.initialize()
+        instance.LUPOS_BUFFER_SIZE = 128
+        if (listOf(".n3", ".ttl", ".nt").contains(inputType)) {
+            LuposdateEndpoint.importTurtleString(instance, inputData)
+        } else {
+            TODO()
+        }
+        val op = LuposdateEndpoint.evaluateSparqlToOperatorgraphA(instance, query)
+        val buf = MyPrintWriter(true)
+        val target = MemoryTable.parseFromAny(targetData, targetType, op.getQuery())!!
+        LuposdateEndpoint.evaluateOperatorgraphToResultA(instance, op, buf, EQueryResultToStreamExt.EMPTY_STREAM)
+        val graph = instance.tripleStoreManager!!.getGraph("")
+        val op2 = graph.getIterator(op.getQuery(), arrayOf(AOPVariable(op.getQuery(), "s"), AOPVariable(op.getQuery(), "p"), AOPVariable(op.getQuery(), "o")), EIndexPatternExt.SPO)
+        val result = (LuposdateEndpoint.evaluateOperatorgraphToResultA(instance, op2, buf, EQueryResultToStreamExt.MEMORY_TABLE) as List<MemoryTable>).first()
+        val buf_err = MyPrintWriter()
+        if (!target.equalsVerbose(result, true, true, buf_err)) {
+            fail(buf_err.toString())
+        }
+        LuposdateEndpoint.close(instance)
+    }
+}
---
+++ src/luposdate3000_launch_code_gen_test/src/jvmTest/kotlin/lupos/launch/code_gen_test/common/resourcessp2bq12csparql32978.kt
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the Luposdate3000 distribution (https://github.com/luposdate3000/luposdate3000).
+ * Copyright (c) 2020-2021, Institute of Information Systems (Benjamin Warnke and contributors of LUPOSDATE3000), University of Luebeck
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package lupos.launch.code_gen_test
+import lupos.endpoint.LuposdateEndpoint
+import lupos.result_format.EQueryResultToStreamExt
+import lupos.shared.MemoryTable
+import lupos.shared.inline.File
+import lupos.shared.inline.MyPrintWriter
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.fail
+
+public class resourcessp2bq12csparql32978 {
+    val inputData = File("src/jvmTest/resources/resourcessp2bq12csparql32978.input").readAsString()
+    val targetData = File("src/jvmTest/resources/resourcessp2bq12csparql32978.output").readAsString()
+    val targetType = ".srx"
+    val inputType = ".n3"
+    val query = File("src/jvmTest/resources/resourcessp2bq12csparql32978.query").readAsString()
+
+    @Ignore
+    @Test
+    fun `resourcessp2bq12csparql32978}`() {
+        val instance = LuposdateEndpoint.initialize()
+        instance.LUPOS_BUFFER_SIZE = 128
+        if (listOf(".n3", ".ttl", ".nt").contains(inputType)) {
+            LuposdateEndpoint.importTurtleString(instance, inputData)
+        } else {
+            TODO()
+        }
+        val op = LuposdateEndpoint.evaluateSparqlToOperatorgraphA(instance, query)
+        val buf = MyPrintWriter(true)
+        val target = MemoryTable.parseFromAny(targetData, targetType, op.getQuery())!!
+        val result = (LuposdateEndpoint.evaluateOperatorgraphToResultA(instance, op, buf, EQueryResultToStreamExt.MEMORY_TABLE) as List<MemoryTable>).first()
+        val buf_err = MyPrintWriter()
+        if (!target.equalsVerbose(result, true, true, buf_err)) {
+            fail(buf_err.toString())
+        }
+        LuposdateEndpoint.close(instance)
+    }
+}
