9b894dd91172fa274e194d113b01dcd242093ac4
Max Manten <max.manten@student.uni-luebeck.de>
2021-07-06 13:41:49 +0200

    Fixed moved Enum generation

--- launcher.main.kts
+++ launcher.main.kts
@@ -1081,7 +1081,7 @@ fun onGenerateEnums() {
         listOf("Turtle2ParserState", "lupos.parser.turtle", "internal", "src/luposdate3000_parser/src/commonMain/kotlin/lupos/parser/turtle/Turtle2ParserState"),
         listOf("EQueryResultToStream", "lupos.result_format", "public", "src/luposdate3000_result_format/src/commonMain/kotlin/lupos/result_format/EQueryResultToStream"),
         listOf("EOptimizerID", "lupos.optimizer.logical", "public", "src/luposdate3000_optimizer_logical/src/commonMain/kotlin/lupos/optimizer/logical/EOptimizerID"),
-        listOf("EVariablePlaceholder", "lupos.code_generator_shared", "internal", "src/luposdate3000_code_generator_shared/src/jvmMain/kotlin/lupos/code_generator_shared/EVariablePlaceholder")
+        listOf("EVariablePlaceholder", "lupos.code_generator_shared", "internal", "src/luposdate3000_code_generator_shared/src/commonMain/kotlin/lupos/code_generator_shared/EVariablePlaceholder")
         )
     for (args in turtleGeneratingArgs) {
         onGenerateEnumsHelper(args[0], args[1], args[2], args[3])
