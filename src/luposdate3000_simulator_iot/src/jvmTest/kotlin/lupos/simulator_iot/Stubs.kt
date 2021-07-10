fd9bcf704c22b2259dbdec584aa1b94c3e2c3f83
Johann <johann_mantler@web.de>
2021-07-10 11:15:57 +0200

    use deterministic test data

--- src/luposdate3000_simulator_iot/src/jvmTest/kotlin/lupos/simulator_iot/Stubs.kt
+++ src/luposdate3000_simulator_iot/src/jvmTest/kotlin/lupos/simulator_iot/Stubs.kt
@@ -1,17 +1,18 @@
 package lupos.simulator_iot
 
+import lupos.simulator_iot.config.Configuration
 import lupos.simulator_iot.geo.GeoLocation
 import kotlin.random.Random
 
 internal object Stubs {
 
     internal fun createEmptyDevice(address: Int): Device {
-        val loc = GeoLocation.getRandom(Random.Default)
+        val loc = GeoLocation.getRandom(Configuration.randomGenerator.random)
         return Device(loc, address, null, null, 100.0, intArrayOf(), -1, true)
     }
 
     internal fun createEmptyDevice(address: Int, linkTypes: IntArray): Device {
-        val loc = GeoLocation.getRandom(Random.Default)
+        val loc = GeoLocation.getRandom(Configuration.randomGenerator.random)
         return Device(loc, address, null, null, 100.0, linkTypes, -1, true)
     }
 }
