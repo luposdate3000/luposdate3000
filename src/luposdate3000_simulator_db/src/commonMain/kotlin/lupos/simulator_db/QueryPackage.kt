ee6d1c834837e54afa139550e842bc0ca3210396
Johann <johann_mantler@web.de>
2021-07-10 21:50:42 +0200

    add queryID

--- src/luposdate3000_simulator_db/src/commonMain/kotlin/lupos/simulator_db/QueryPackage.kt
+++ src/luposdate3000_simulator_db/src/commonMain/kotlin/lupos/simulator_db/QueryPackage.kt
@@ -1,6 +1,10 @@
 package lupos.simulator_db
 
-public class QueryPackage(public val sourceAddress: Int, public val query: ByteArray) : IDatabasePackage {
+public class QueryPackage(
+    public val sourceAddress: Int,
+    public val query: ByteArray,
+    public val queryID: Int = -1) : IDatabasePackage {
+
     public override fun getPackageSizeInBytes(): Int {
         return query.size + 4
     }
--- src/luposdate3000_simulator_db/src/commonMain/kotlin/lupos/simulator_db/QueryResponsePackage.kt
+++ src/luposdate3000_simulator_db/src/commonMain/kotlin/lupos/simulator_db/QueryResponsePackage.kt
@@ -1,6 +1,9 @@
 package lupos.simulator_db
 
-public class QueryResponsePackage(public val result: ByteArray) : IDatabasePackage {
+public class QueryResponsePackage(
+    public val result: ByteArray,
+    public val queryID: Int = -1) : IDatabasePackage {
+
     public override fun getPackageSizeInBytes(): Int {
         return result.size
     }
