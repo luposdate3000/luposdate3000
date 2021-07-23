a8b70111668e0e8232988ae6f0b27c80f68ac23c
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-07-23 11:40:56 +0000

    a

--- src/luposdate3000_spa_client/build.template.gradle.kts
+++ src/luposdate3000_spa_client/build.template.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 
 evaluationDependsOn(":src:luposdate3000_endpoint")
 task<Exec>("bowerInstall") {
-dependsOn("npmInstall")
-commandLine("./node_modules/.bin/bower","install", "--allow-root")
+    dependsOn("npmInstall")
+    commandLine("./node_modules/.bin/bower", "install", "--allow-root")
 }
 task<Exec>("build") {
     dependsOn("bowerInstall")
@@ -13,14 +13,3 @@ task<Exec>("build") {
     workingDir("../..")
     commandLine("./launcher.main.kts", "--copySPAClient")
 }
-
-/*
-task npmInstall(type: Exec) {
-    workingDir 'src/main/webapp'
-    commandLine 'npm', 'install'
-}
-*/
-//   https://github.com/srs/gradle-node-plugin/blob/master/docs/node.md
-//   https://gist.github.com/spikeheap/8558786
-
-// https://github.com/node-gradle/gradle-node-plugin/blob/master/examples/simple-node/npm/build.gradle
