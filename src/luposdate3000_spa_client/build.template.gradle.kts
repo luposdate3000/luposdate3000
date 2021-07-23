ea084460899f8904eaac2b102078799dc45b6dc9
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-07-23 18:19:59 +0000

    a

--- src/luposdate3000_spa_client/build.template.gradle.kts
+++ src/luposdate3000_spa_client/build.template.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
-    id("com.github.node-gradle.node") version "3.1.0"
+    id("org.ysb33r.nodejs.base") version "0.10.0-alpha.2"
 }
 
 evaluationDependsOn(":src:luposdate3000_endpoint")
-
+var executableDirectory = ""
+nodejs {
+    executableByVersion("12.21.0")
+    executableDirectory = executable.get().toString()
+    executableDirectory = executableDirectory.substring(0, executableDirectory.length - 4) // remove the trailing program name
+}
 task<Exec>("downloadInstruments") {
     commandLine("git", "clone", "--depth=1", "https://github.com/nbrosowsky/tonejs-instruments.git")
 }
@@ -12,8 +17,13 @@ task("downloadInstrumentsIfNotExist") {
         dependsOn("downloadInstruments")
     }
 }
+task<Exec>("npmInstall") {
+    environment["PATH"] = executableDirectory + ":" + environment["PATH"]
+    commandLine(executableDirectory + "npm", "install")
+}
 task<Exec>("bowerInstall") {
     dependsOn("npmInstall")
+    environment["PATH"] = executableDirectory + ":" + environment["PATH"]
     commandLine("./node_modules/.bin/bower", "install", "--allow-root")
 }
 task<Exec>("build") {
@@ -21,5 +31,16 @@ task<Exec>("build") {
     dependsOn("downloadInstrumentsIfNotExist")
     mustRunAfter(":src:luposdate3000_endpoint:build")
     workingDir("../..")
+    environment["PATH"] = executableDirectory + ":" + environment["PATH"]
     commandLine("./launcher.main.kts", "--copySPAClient")
 }
+
+/*
+plugins {
+    id("org.ysb33r.nodejs.base") version "0.10.0-alpha.2"
+    id("org.ysb33r.nodejs.npm") version "0.10.0-alpha.2"
+}
+//https://ysb33rorg.gitlab.io/nodejs-gradle-plugin/0.10.0-alpha.2/docs/product-documentation.html
+task<org.ysb33r.gradle.nodejs.tasks.NpmPackageJsonInit>("x"){ 
+}
+*/
