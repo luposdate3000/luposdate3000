2aa10f6aee17739aa938ac70e8545fa25cd82e02
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-08-11 07:47:29 +0000

    backup

--- src/luposdate3000_simulator_iot/src/commonMain/kotlin/lupos/simulator_iot/config/JsonParser.kt
+++ src/luposdate3000_simulator_iot/src/commonMain/kotlin/lupos/simulator_iot/config/JsonParser.kt
@@ -19,17 +19,15 @@ package lupos.simulator_iot.config
 
 public class JsonParser {
     private fun readValueAt(data: String, off: Int): Pair<Int, Any> {
-        var i = off
+        var i = readSpacesAt(data, off)
         while (i <data.length) {
-            val c = data[i]
-            when (c) {
+            when (data[i]) {
                 '{' -> return readMapAt(data, i)
                 '[' -> return readArrayAt(data, i)
                 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '.' -> return readNumberAt(data, i)
                 '"' -> return readStringAt(data, i)
                 't', 'f', 'T', 'F' -> return readBooleanAt(data, i)
-                ' ', '\t', '\n', '\r' -> { i++ }
-                else -> TODO("unknown char at $off")
+                else -> TODO("unknown char at $i")
             }
         }
         return data.length to mutableMapOf<String, Any>()
@@ -37,36 +35,38 @@ public class JsonParser {
     private fun readMapAt(data: String, off: Int): Pair<Int, MutableMap<String, Any>> {
         var i = off + 1
         val res = mutableMapOf<String, Any>()
-
-        var state = 0 // 0-> readKey, 1->readCollon, 2->readValue,3->readCommaOrEnd
-        var key = ""
-
-        while (i <data.length) {
-            val c = data[i]
-            when (c) {
+        loop@ while (i <data.length) {
+            i = readSpacesAt(data, i)
+            when (data[i]) {
                 '}' -> return (i + 1) to res
-                ' ', '\t', '\n', '\r' -> { i++ }
                 '"' -> {
-                    val (j, tmp) = readStringAt(data, i)
-                    i = j
-                    when (state) {
-                        0 -> {
-                            key = tmp
-                            state = 1
-                        }
-                        2 -> {
-                            res[key] = tmp
-                            state = 3
-                        }
-                        else -> {
-                            TODO("unknown char at $off")
-                        }
+                    val (j, key) = readStringAt(data, i)
+                    i = readSpacesAt(data, j)
+                    if (data[i] != ':') {
+                        TODO("unknown char at $i")
+                    }
+                    i = readSpacesAt(data, i + 1)
+                    val (k, value) = readValueAt(data, i)
+                    res[key] = value
+                    i = readSpacesAt(data, k)
+                    when (data[i]) {
+                        ',' -> i++
+                        '}' -> return (i + 1)to res
                     }
                 }
-                else -> TODO("unknown char at $off")
             }
         }
         TODO("object not closed")
+    }
+    private fun readSpacesAt(data: String, off: Int): Int {
+        var i = off
+        while (i <data.length) {
+            when (data[i]) {
+                ' ', '\t', '\n', '\r' -> { i++ }
+                else -> return i
+            }
+        }
+        return i
     }
     private fun readArrayAt(data: String, off: Int): Pair<Int, MutableList<Any>> {
         TODO()
