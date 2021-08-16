a1f6da4a300bb3620fdaa87210d597ce413e0f43
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-08-16 06:30:11 +0000

    a

--- launcher.main.kts
+++ launcher.main.kts
@@ -42,7 +42,7 @@ import launcher.ParamClassMode
 import launcher.ReleaseMode
 import launcher.SuspendMode
 import launcher.TargetMode2
-import launcher.createBuildFileForModule
+import launcher.createBuildFileForModule 
 import launcher.fixPathNames
 import launcher.targetModeCompatible
 import lupos.shared.EGarbageCollectorExt
