5992e627c120cd23710dc1d46c6ea52b09a86dfa
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2021-12-14 13:03:50 +0000

    fix bug series in tests

--- src/luposdate3000_code_gen_test_00/src/jvmTest/kotlin/lupos/code_gen_test_00/MAXwithGROUPBY.kt
+++ src/luposdate3000_code_gen_test_00/src/jvmTest/kotlin/lupos/code_gen_test_00/MAXwithGROUPBY.kt
@@ -1369,14 +1369,14 @@ public class MAXwithGROUPBY {
     public fun simulatorHelper(fileName: String, database_cfg: MutableMap<String, Any>, routingProtocol: String) {
         val simRun = SimulationRun()
         val config = simRun.parseConfig(
-            fileName,
-            false,
+            fileName, false,
             {
                 it.getOrEmptyObject("deviceType").getOrEmptyObject("LUPOSDATE_DEVICE").getOrEmptyObject("applications").getOrEmptyObject("lupos.simulator_db.luposdate3000.ApplicationFactory_Luposdate3000").putAll(database_cfg)
                 it.getOrEmptyObject("routing").putAll(mapOf("protocol" to routingProtocol))
             }
         )
 
+        simRun.entities = config.getEntities()
         simRun.maxClock = if (simRun.maxClock == simRun.notInitializedClock) simRun.maxClock else simRun.maxClock
         simRun.steadyClock = if (simRun.steadyClock == simRun.notInitializedClock) simRun.steadyClock else simRun.steadyClock
         simRun.startUp()
