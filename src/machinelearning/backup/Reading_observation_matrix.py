5c6a4793e392a9ee785ff881cb7648a28d024aa9
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2022-06-08 13:31:02 +0000

    removing backup

--- src/machinelearning/backup/Reading_observation_matrix.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-f = open('Observation_matrix','rb')
-while 1:
-    try:
-        print(pickle.load(f))
-    except EOFError:
-        break
--- src/machinelearning/backup/testing.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-file = open("15j_100_3:7_4_triples.pkl",'rb')
-best_percentages = pickle.load(file)
-worst_percentages = pickle.load(file)
-print(best_percentages)
-print(worst_percentages)
