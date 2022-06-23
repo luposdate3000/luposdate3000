7a9ae1b184d73686c16b152ff9361de6a476e03b
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2022-06-23 11:40:43 +0000

    a

--- src/machinelearning/13_obtain_results.py
+++ src/machinelearning/13_obtain_results.py
@@ -8,7 +8,7 @@ import mysql.connector
 from database_env import DatabaseEnv
 from stable_baselines3 import PPO
 
-sqlquery = """SELECT (bv.value/minmax.mymin) as score from
+sqlquery = """SELECT (if(bv.value is null,minmax.mymax,bv.value)/minmax.mymin) as score from
  (select min(value) as mymin , max(value) as mymax,query_id from benchmark_values group by query_id) as minmax,
  mapping_join mj,
  optimizer_choice oc,
@@ -36,6 +36,15 @@ rows = cursor.fetchall()
 triplePatterns = [x[0] for x in rows]
 cursor.execute("select name,id from mapping_optimizer")
 optimizers = cursor.fetchall()
+'''
+training steps
+trained on
+evaluated on
+
+totalscore
+'''
+scoreMap = {}
+trainedOnMap = []
 
 for triplePattern in triplePatterns:
     for optimizer in optimizers:
@@ -45,8 +54,11 @@ for triplePattern in triplePatterns:
         datapoints.append([0, 0])
         cursor.execute(sqlquery, (triplePattern, int(optimizer[1])))
         rows = cursor.fetchall()
+        total_score = 0
         for row in rows:
             score = float(row[0])
+            if score < 10:
+                total_score = idx / len(rows)
             if last is None:
                 last = score
             elif last < score:
@@ -55,6 +67,51 @@ for triplePattern in triplePatterns:
             idx += 1
         if last is not None:
             datapoints.append([idx / len(rows), last])
-            with open("measurements_" + optimizer[0] + "_" + str(triplePattern) + ".csv", "w", newline="") as f:
+            name = optimizer[0]
+            if name.startswith("model_"):
+                tmp = name[:-len(".model")].split("_")
+                training_steps = tmp[-1]
+                trained_on = "_".join(tmp[1:-1])
+                print("key", triplePattern, training_steps, trained_on, total_score)
+                scoreMap.setdefault(triplePattern, {}).setdefault(training_steps, {})[trained_on] = total_score
+                trainedOnMap.append(trained_on)
+            else:
+                print("key", triplePattern, "luposdate", total_score)
+            with open("measurements_" + name + "_" + str(triplePattern) + ".csv", "w", newline="") as f:
                 writer = csv.writer(f)
                 writer.writerows(datapoints)
+
+for evaluatedOn, tmp1 in scoreMap.items():
+    print("figurename", evaluatedOn)
+    rows = []
+    rows.append(["x"].extend(list(dict.fromkeys(trainedOnMap))))
+    for steps, tmp2 in tmp1.items():
+        row = [-1] * len(rows[0])
+        row[0] = steps
+        for trainedOn, score in tmp2.items():
+            row[rows[0].index(trainedOn)] = score
+        rows.append(row)
+    with open("figure_" + evaluatedOn + ".csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+'''
+36x
+- fixiere trained-on
+- fixiere evaluated-on
+vergleiche training steps
+
+
+
+3x
+- fixiere evaluated-on = 32,64,128
+- fixiere training-staps = best-of
+vergleiche trained-on
+
+----
+
+y-achse score
+x-achse steps
+graphen trained-on
+figuren evaluated-on
+
+'''
