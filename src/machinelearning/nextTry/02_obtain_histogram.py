8ea6727c0afaecf862550a1ef0d22f38f93a1b91
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2022-06-29 09:27:09 +0000

    a

--- src/machinelearning/nextTry/02_obtain_histogram.py
+++ src/machinelearning/nextTry/02_obtain_histogram.py
@@ -17,42 +17,48 @@ cursor = db.cursor()
 cursor.execute("SELECT name, id FROM mapping_dictionary")
 rows = cursor.fetchall()
 dictionary = []
-with open(filename + "dictionary", "r") as f:
+with open(filename + ".dictionary", "r") as f:
     dictionary = f.read().split('\n')
 print("loaded dictionary")
 dictionaryMapping = {}  # db -> dataset-dict-id
 dictionaryMappingValues = {}  # dataset-dict-id -> triples
 for row in rows:
     x = row[1]
-    y = dictionary.indexOf(row[0])
+    y = dictionary.index(row[0])
+    print("mapping",x,y,row[0])
     dictionaryMapping[x] = y
     dictionaryMappingValues[y] = []
 print("has dictionary mapping")
 with open(filename + ".data", "r") as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
-        if row[1] in dictionaryMappingValues:
-            data = dictionaryMappingValues[row[1]]
-            data.append((row[0], row[2]))
+        if int(row[1]) in dictionaryMappingValues:
+            data = dictionaryMappingValues[int(row[1])]
+            data.append((int(row[0]), int(row[2])))
 print("loaded and filtered data")
 
 
 def getHistogramForColumn(col,dat,key):
  data=sorted(dat, key=lambda tup: tup[col])
- datamin=data[0][col]
+ datamin=0
+ try:
+  datamin=data[0][col]
+ except:
+  print("error",len(data),key)
+  print(len(data[0]),col)
  datamax=data[-1][col]+1
  histogram=[key,col,buckets]
  if mode == "equi-width":
-  stepsize=(datamax-datamin)/buckets
+  stepsize=max(1,int((datamax-datamin)/buckets))
   right=datamin
   idx=0
   for i in range(buckets):
    left=right
    right=left+stepsize
    ctr=0
-   while idx<len(data) && data[idx][col]<right:
-    idx++
-    ctr++
+   while idx<len(data) and data[idx][col]<right:
+    idx+=1
+    ctr+=1
    histogram.append(left)
    histogram.append(right)
    histogram.append(ctr)
@@ -60,5 +66,5 @@ def getHistogramForColumn(col,dat,key):
 
 with open(filename + ".histograms", "w") as f:
  for i, v in dictionaryMapping.items():
-  f.write(",".join(getHistogramForColumn(0,dictionaryMappingValues[v],i)))
-  f.write(",".join(getHistogramForColumn(2,dictionaryMappingValues[v],i)))
+  f.write(",".join([str(x) for x in getHistogramForColumn(0,dictionaryMappingValues[v],i)])+"\n")
+  f.write(",".join([str(x) for x in getHistogramForColumn(1,dictionaryMappingValues[v],i)])+"\n")
