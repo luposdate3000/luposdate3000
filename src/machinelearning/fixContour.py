c0453b17d667829b3212ffc57a7b14aed3578195
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2022-07-06 14:14:56 +0000

    a

--- src/machinelearning/fixContour.py
+++ src/machinelearning/fixContour.py
@@ -2,13 +2,17 @@
 import csv
 import sys
 
-with open("cont.dat","r") as f:
+with open('cont2.dat', 'w') as out:
+ with open("cont.dat","r") as f:
     for line in f:
      if line.startswith("#"):
-      print (line)
-     elif len(line)==0:
-      print(line)
+      out.write (line+"\n")
      else:
-      print(line.split(" "))
+      ll=[float(i) for i in list(filter(str.strip, line.split(" ")))]
+      if len(ll)<3:
+       out.write ("\n")
+      else:
+       ll[0]=max(0,min(13.5,ll[0]-2))
+       ll[1]=max(0,min(13.5,ll[1]-2))
+       out.write(" "+" ".join(str(x) for x in ll)+"\n")
 
-#"cont2.dat"
