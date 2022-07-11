b4403cd9bf3e0d11c12204221e09c8d7539b9150
Benjamin Warnke <warnke@ifis.uni-luebeck.de>
2022-07-11 12:08:42 +0000

    a

--- dynamic-programing.main.kts
+++ dynamic-programing.main.kts
@@ -9,50 +9,39 @@ import kotlin.math.pow
 
 val histograms = arrayOf(
     812 to mapOf("x1" to 200), //0
-    821 to mapOf("x1" to 300, "x2" to 423),//1
-    823 to mapOf("x2" to 423),//2
-    853 to mapOf("x3" to 850),//3
-    834 to mapOf("x2" to 300),//4
-    945 to mapOf("x2" to 930),//5
-    834 to mapOf("x1" to 30),//6
+    853 to mapOf("x1" to 850),//1
+    821 to mapOf("x1" to 300, "x2" to 423),//2
+    823 to mapOf("x1" to 423),//3
+    834 to mapOf("x1" to 300),//4
+    834 to mapOf("x3" to 30),//6
+    945 to mapOf("x1" to 930),//5
 )
 
 
 var SS: Int = 2.0.pow(histograms.size).toInt() - 1
-val arrCost = DoubleArray(SS + 1) { -1.0 }
+val arrCostLast = DoubleArray(SS + 1) { -1.0 }
+val arrCostSum = DoubleArray(SS + 1) { -1.0 }
 val arrLeft = IntArray(SS + 1)
 val arrRight = IntArray(SS + 1)
-val globalFactorList = mutableListOf<Pair<Int, Double>>()
+
+val variables = mutableListOf<String>()
+var counterMap = Array(1) { IntArray(1) }
 
 fun initialize() {
-    val mapVarnameToInput = mutableMapOf<String, MutableSet<Int>>()
     var x = 1
-    val mapIntToBitmask = IntArray(histograms.size)
+    var variablesSet = mutableSetOf<String>()
     for (i in 0 until histograms.size) {
-        arrCost[x] = histograms[i].first.toDouble()
-        mapIntToBitmask[i] = x
-        for (k in histograms[i].second.keys) {
-            mapVarnameToInput.getOrPut(k, { mutableSetOf(i) }).add(i)
-        }
+        variablesSet.addAll(histograms[i].second.keys)
+        arrCostLast[x] = histograms[i].first.toDouble()
+        arrCostSum[x] = histograms[i].first.toDouble()
         x *= 2
     }
-    for ((inputVariable, inputSet) in mapVarnameToInput) {
-        if (inputSet.size > 1) {
-            val inputList = inputSet.toIntArray()
-            for (i in 0 until inputList.size) {
-                for (j in i + 1 until inputList.size) {
-                    val ii = inputList[i]
-                    val jj = inputList[j]
-                    val count1 = histograms[ii].first.toDouble()
-                    val count2 = histograms[jj].first.toDouble()
-                    val distinct1 = histograms[ii].second[inputVariable]!!.toDouble()
-                    val distinct2 = histograms[jj].second[inputVariable]!!.toDouble()
-                    val factorOld = (count1 / distinct1) * (count2 / distinct2) * (min(distinct1, distinct2) / max(distinct1, distinct2))
-                    val factor = 1 - ((max(distinct1, distinct2) - min(distinct1, distinct2)) / max(distinct1, distinct2))
-                    globalFactorList.add(mapIntToBitmask[ii] + mapIntToBitmask[jj] to factor)
-                    println("globalFactorList ${inputVariable} ${mapIntToBitmask[ii].toString(2).padStart(8, '0')} ${mapIntToBitmask[jj].toString(2).padStart(8, '0')} = $factor")
-                }
-            }
+    variables.addAll(variablesSet)
+    counterMap = Array(variables.size) { IntArray(histograms.size) { -1 } }
+    for (i in 0 until histograms.size) {
+        for ((variable, distinct) in histograms[i].second) {
+            val j = variables.indexOf(variable)
+            counterMap[j][i] = distinct
         }
     }
 }
@@ -71,22 +60,59 @@ initialize()
 println("initialization done                                    in ${(System.nanoTime() - starttime).toDouble() / 1_000_000_000.0}s")
 
 var ctr = 0
-fun enumerateThem(S: Int, factorList: List<Pair<Int, Double>>) {
+fun enumerateThem(S: Int) {
     var S1 = S and (-S)
     do {
         var S2 = S - S1
-        if (arrCost[S1] < 0.0) {
-            enumerateThem(S1, factorList.filter { it.first == it.first and S1 })
+        if (arrCostSum[S1] < 0.0) {
+            enumerateThem(S1)
         }
-        if (arrCost[S2] < 0.0) {
-            enumerateThem(S2, factorList.filter { it.first == it.first and S2 })
+        if (arrCostSum[S2] < 0.0) {
+            enumerateThem(S2)
         }
-        var cost = arrCost[S1] * arrCost[S2]
-        for (factor in factorList.filter { (it.first != (it.first and S1)) && (0 != (it.first and S1)) }) {
-            cost *= factor.second
+        var cost = arrCostLast[S1] * arrCostLast[S2]
+        for (v in 0 until variables.size) {
+            var min1 = -1.0
+            var min2 = -1.0
+            var i = S1
+            var ii = 0
+            while (i > 0) {
+                if (i and 0x1 != 0) {
+                    val x = counterMap[v][ii]
+                    if ((min1 < 0 || x < min1) && (x >= 0)) {
+                        min1 = 0.0 + x
+                    }
+                }
+                i = i / 2
+                ii++
+            }
+            i = S2
+            ii = 0
+            while (i > 0) {
+                if (i and 0x1 != 0) {
+                    val x = counterMap[v][ii]
+                    if ((min1 < 0 || x < min1) && (x >= 0)) {
+                        min2 = 0.0 + x
+                    }
+                }
+                i = i / 2
+                ii++
+            }
+            if (S == 0b01110001) {
+                println("reducing ${min1 >= 0 && min2 >= 0} ${S1.toString(2).padStart(8, '0')} ${S2.toString(2).padStart(8, '0')} due to ${variables[v]} .. ${min(min1, min2)}*(${arrCostLast[S1] / min1}*${arrCostLast[S2] / min2})")
+            }
+            if (min1 >= 0 && min2 >= 0) {
+                val cost2 = min(min1, min2) * ((arrCostLast[S1] / min1) * (arrCostLast[S2] / min2))
+                cost = min(cost, cost2)
+            }
         }
-        if (arrCost[S] < 0 || cost < arrCost[S]) {
-            arrCost[S] = cost
+        val costSum = cost + arrCostSum[S1] + arrCostSum[S2]
+        if (S == 0b01110001) {
+            println("choice ${S1.toString(2).padStart(8, '0')} ${S2.toString(2).padStart(8, '0')} => $costSum")
+        }
+        if (arrCostSum[S] < 0 || costSum < arrCostSum[S]) {
+            arrCostLast[S] = cost
+            arrCostSum[S] = costSum
             arrLeft[S] = S1
             arrRight[S] = S2
         }
@@ -100,10 +126,10 @@ fun enumerateThem(S: Int, factorList: List<Pair<Int, Double>>) {
 
 
 
-enumerateThem(SS, globalFactorList)
+enumerateThem(SS)
 
 for (i in 0 until SS + 1) {
-//println("${i.toString(2).padStart(8,'0')} => ${arrCost[i]}")
+    println("${i.toString(2).padStart(8, '0')} => ${arrCostSum[i]} -- ${arrCostLast[i]} .. ${joinOrderToString(i)}")
 }
 
 
