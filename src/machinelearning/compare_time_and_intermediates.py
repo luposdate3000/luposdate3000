f828270245b5fbb59ad6107b3fe4381ba5fd1440
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2022-10-04 07:57:56 +0000

    a

---
+++ src/machinelearning/compare_time_and_intermediates.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S python3 -OO -u
+import mysql.connector
+db = mysql.connector.connect(host="localhost", user="machinelearningbenchmarks", password="machinelearningbenchmarks", database="machinelearningbenchmarks")
+cursor = db.cursor()
+
+for i in range(1,18):
+ low=2**(i-1)+1
+ high=2**i
+ if low==2:
+  low=0
+
+ cursor.execute("select SUM(ctr) as sum_dist_from_avg_1000 from( select count(*) as ctr from benchmark_values bv2 join (select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=%s and bv1.valuetime<=%s group by bv1.dataset_id,bv1.query_id) as bv3 on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where (((bv2.value/bv2.valuetime)/a>1.2) OR (a/(bv2.value/bv2.valuetime))>1.2) and c >1 group by bv3.dataset_id,bv3.query_id )as bv4;",(low,high))
+ value_actual = cursor.fetchone()[0]
+
+ cursor.execute("select SUM(ctr) as sum_all_for_avg_1000 from( select count(*) as ctr from benchmark_values bv2 join (select bv1.dataset_id,bv1.query_id,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=%s and bv1.valuetime<=%s group by bv1.dataset_id,bv1.query_id) as bv3 on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where c >1 group by bv3.dataset_id,bv3.query_id)as bv4;",(low,high))
+ value_max = cursor.fetchone()[0]
+
+ data=None
+ if value_actual is not None:
+  data=value_actual/value_max
+ print(low,high,data)
--- src/machinelearning/compare_time_and_intermediates.sql
+++ src/machinelearning/compare_time_and_intermediates.sql
@@ -1,21 +1,9 @@
 use machinelearningbenchmarks;
 
-select SUM(ctr) as sum_larger_than_avg from(
-select count(*) as ctr from benchmark_values bv2 join
-(select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null group by bv1.dataset_id,bv1.query_id) as bv3
-on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where bv2.value/bv2.valuetime>a and c >1 group by bv3.dataset_id,bv3.query_id
-)as bv4;
-
-select SUM(ctr) as sum_smaller_than_avg from(
-select count(*) as ctr from benchmark_values bv2 join
-(select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null group by bv1.dataset_id,bv1.query_id) as bv3
-on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where bv2.value/bv2.valuetime<a and c >1 group by bv3.dataset_id,bv3.query_id
-)as bv4;
-
 select SUM(ctr) as sum_dist_from_avg from(
 select count(*) as ctr from benchmark_values bv2 join
 (select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null group by bv1.dataset_id,bv1.query_id) as bv3
-on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where (((bv2.value/bv2.valuetime)/a>1.6) OR (a/(bv2.value/bv2.valuetime))>1.6) and c >1 group by bv3.dataset_id,bv3.query_id
+on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where (((bv2.value/bv2.valuetime)/a>1.2) OR (a/(bv2.value/bv2.valuetime))>1.2) and c >1 group by bv3.dataset_id,bv3.query_id
 )as bv4;
 
 select SUM(ctr) as sum_all_for_avg from(
@@ -25,29 +13,40 @@ on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where c >1 grou
 )as bv4;
 
 
-
-select SUM(ctr) as sum_larger_than_avg from(
+select SUM(ctr) as sum_dist_from_avg_1000 from(
 select count(*) as ctr from benchmark_values bv2 join
 (select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=1000 group by bv1.dataset_id,bv1.query_id) as bv3
-on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where bv2.value/bv2.valuetime>a and c >1 group by bv3.dataset_id,bv3.query_id
+on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where (((bv2.value/bv2.valuetime)/a>1.2) OR (a/(bv2.value/bv2.valuetime))>1.2) and c >1 group by bv3.dataset_id,bv3.query_id
 )as bv4;
 
-select SUM(ctr) as sum_smaller_than_avg from(
-select count(*) as ctr from benchmark_values bv2 join
-(select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=1000 group by bv1.dataset_id,bv1.query_id) as bv3
-on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where bv2.value/bv2.valuetime<a and c >1 group by bv3.dataset_id,bv3.query_id
-)as bv4;
-
-select SUM(ctr) as sum_dist_from_avg from(
-select count(*) as ctr from benchmark_values bv2 join
-(select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=1000 group by bv1.dataset_id,bv1.query_id) as bv3
-on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where (((bv2.value/bv2.valuetime)/a>1.6) OR (a/(bv2.value/bv2.valuetime))>1.6) and c >1 group by bv3.dataset_id,bv3.query_id
-)as bv4;
-
-select SUM(ctr) as sum_all_for_avg from(
+select SUM(ctr) as sum_all_for_avg_1000 from(
 select count(*) as ctr from benchmark_values bv2 join
 (select bv1.dataset_id,bv1.query_id,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=1000 group by bv1.dataset_id,bv1.query_id) as bv3
 on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where c >1 group by bv3.dataset_id,bv3.query_id
 )as bv4;
 
 
+select SUM(ctr) as sum_dist_from_avg_2000 from(
+select count(*) as ctr from benchmark_values bv2 join
+(select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=2000 group by bv1.dataset_id,bv1.query_id) as bv3
+on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where (((bv2.value/bv2.valuetime)/a>1.2) OR (a/(bv2.value/bv2.valuetime))>1.2) and c >1 group by bv3.dataset_id,bv3.query_id
+)as bv4;
+
+select SUM(ctr) as sum_all_for_avg_2000 from(
+select count(*) as ctr from benchmark_values bv2 join
+(select bv1.dataset_id,bv1.query_id,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=2000 group by bv1.dataset_id,bv1.query_id) as bv3
+on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where c >1 group by bv3.dataset_id,bv3.query_id
+)as bv4;
+
+
+select SUM(ctr) as sum_dist_from_avg_10000 from(
+select count(*) as ctr from benchmark_values bv2 join
+(select bv1.dataset_id,bv1.query_id,AVG(bv1.value/bv1.valuetime) AS a,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=10000 group by bv1.dataset_id,bv1.query_id) as bv3
+on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where (((bv2.value/bv2.valuetime)/a>1.2) OR (a/(bv2.value/bv2.valuetime))>1.2) and c >1 group by bv3.dataset_id,bv3.query_id
+)as bv4;
+
+select SUM(ctr) as sum_all_for_avg_10000 from(
+select count(*) as ctr from benchmark_values bv2 join
+(select bv1.dataset_id,bv1.query_id,COUNT(*) as c from benchmark_values bv1 where bv1.value is not null and bv1.valuetime is not null and bv1.valuetime>=10000 group by bv1.dataset_id,bv1.query_id) as bv3
+on bv3.dataset_id=bv2.dataset_id and bv3.query_id = bv2.query_id where c >1 group by bv3.dataset_id,bv3.query_id
+)as bv4;
