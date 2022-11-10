a05598eb222269a4f17ff8d20c75dc5b86158808
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2022-11-10 13:19:12 +0000

    a

--- src/luposdate3000_result_format/src/commonMain/kotlin/lupos/result_format/QueryResultToXMLStream.kt
+++ src/luposdate3000_result_format/src/commonMain/kotlin/lupos/result_format/QueryResultToXMLStream.kt
@@ -166,7 +166,7 @@ public class QueryResultToXMLStream : IResultFormat {
             val columnNames: List<String>
             if (columnProjectionOrder.isNotEmpty()) {
                 columnNames = columnProjectionOrder
-                SanityCheck.check({ /*SOURCE_FILE_START*/"/src/luposdate3000/src/luposdate3000_result_format/src/commonMain/kotlin/lupos/result_format/QueryResultToXMLStream.kt:169"/*SOURCE_FILE_END*/ }, { child.names.toSet().containsAll(columnNames) }, { "${columnNames.map { it }} vs ${child.names}" })
+                SanityCheck.check({ /*SOURCE_FILE_START*/"/src/luposdate3000/src/luposdate3000_result_format/src/commonMain/kotlin/lupos/result_format/QueryResultToXMLStream.kt:168"/*SOURCE_FILE_END*/ }, { child.names.toSet().containsAll(columnNames) }, { "${columnNames.map { it }} vs ${child.names}" })
             } else {
                 columnNames = child.names.toList()
             }
