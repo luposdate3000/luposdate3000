c90012e0f6c209e05cdbd963a376c6fa444fdb21
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2022-11-21 09:28:13 +0000

    a

--- operator-distribution-visual.py
+++ operator-distribution-visual.py
@@ -2,23 +2,65 @@
 import os
 
 foldernames = []
-headline = {}
-headline["Full16DB"] = "topology"
-headline["distributed"] = "additionalHops"
-headline["routing_AllShortestPath"] = "routing"
-headline["operator-distribution-test-optimizer-topology-assisted"] = "distribution algorithm"
+headlineHelper = {}
+headlineHelper["Full16DB"] = "topology"
+headlineHelper["distributed"] = "additionalHops"
+headlineHelper["routing_AllShortestPath"] = "routing"
+headlineHelper["operator-distribution-test-optimizer-topology-assisted"] = "distribution algorithm"
+headlineHelper["_by_id_S_all_collations"] = "partitioning"
+header = {}
 for folder, subs, files in os.walk("simulator_output"):
     parts = folder.split("/")
     if "measurement.csv" in files:
-        print(len(parts))
         if len(foldernames) == 0:
             for x in parts:
                 foldernames.append({x})
         elif len(foldernames) != len(parts):
+            print("fail 1")
             exit(1)
         else:
             for i in range(len(parts)):
                 foldernames[i].add(parts[i])
+        with open(folder + "/measurement.csv") as f:
+            line = f.readline().strip()
+            row = line.split(",")
+            for r in row:
+                header[r] = r
 for i in range(len(foldernames)):
     if len(foldernames[i]) > 1:
-        print(i, foldernames[i])
+        flag = False
+        for f in foldernames[i]:
+            if f in headlineHelper:
+                header["folder_part_" + str(i)] = headlineHelper[f]
+                flag = True
+        if not flag:
+            print("fail 2", foldernames[i], headlineHelper)
+            exit(1)
+headLine = []
+for k, v in header.items():
+    headLine.append(v)
+print(",".join(headLine))
+
+headLine = []
+for k, v in header.items():
+    headLine.append(k)
+
+for folder, subs, files in os.walk("simulator_output"):
+    parts = folder.split("/")
+    if "measurement.csv" in files:
+        with open(folder + "/measurement.csv") as f:
+            myvalues = {}
+            for i in range(len(parts)):
+                myvalues["folder_part_" + str(i)] = parts[i]
+            firstheaders = None
+            for line in f:
+                row = line.strip().split(",")
+                if firstheaders is None:
+                    firstheaders = row
+                else:
+                    for i in range(len(firstheaders)):
+                        myvalues[firstheaders[i]] = row[i]
+                    row = []
+                    for h in headLine:
+                        row.append(myvalues[h])
+                    print(",".join(row))
