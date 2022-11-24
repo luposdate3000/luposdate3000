9080010170289592d0f669da481e16135f9e7039
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2022-11-24 07:55:58 +0000

    a

--- operator-distribution-test.sh
+++ operator-distribution-test.sh
@@ -5,43 +5,48 @@ cmd=$(./launcher.main.kts --dryMode=Enable --run --mainClass=Launch_Simulator_Co
 
 for topology in src/luposdate3000_simulator_db/src/jvmMain/resources/topology/*16DB.json
 do
-#for dataDistribution in src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_key.json src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_id_S_all_collations.json
-for dataDistribution in src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_id_S_all_collations.json
+for dataDistribution in src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_key.json src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_id_S_all_collations.json
 do
 for programDistribution in src/luposdate3000_simulator_db/src/jvmMain/resources/programDistribution/distributed*
 do
-for routing in src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_AllShortestPath.json src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_RPL_Fast.json
+for routing in src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_AllShortestPath.json
+#for routing in src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_AllShortestPath.json src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_RPL_Fast.json
 do
 for optimizer in operator-distribution-test-optimizer-topology-assisted.json operator-distribution-test-optimizer-default.json
 do
+for sosa in src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID.json src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID100.json
+do
+for joindistributionlocation in src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_distribution*
+do
 echo $cmd \
-src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID.json \
+$sosa \
 $topology \
 $programDistribution \
 src/luposdate3000_simulator_db/src/jvmMain/resources/queries/SOSA_Queries.json \
 $dataDistribution \
 src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000.json \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_distribution_routing.json \
+$joindistributionlocation \
 src/luposdate3000_simulator_db/src/jvmMain/resources/multicast/luposdate3000MulticastEnabled.json \
 $routing \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_local_execution_disabled.json \
+src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_local_execution_enabled.json \
 $optimizer \
 operator-distribution-test.json
 
 $cmd \
-src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID.json \
+$sosa \
 $topology \
 $programDistribution \
 src/luposdate3000_simulator_db/src/jvmMain/resources/queries/SOSA_Queries.json \
 $dataDistribution \
 src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000.json \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_distribution_routing.json \
+$joindistributionlocation \
 src/luposdate3000_simulator_db/src/jvmMain/resources/multicast/luposdate3000MulticastEnabled.json \
 $routing \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_local_execution_disabled.json \
+src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_local_execution_enabled.json \
 $optimizer \
 operator-distribution-test.json
-
+done
+done
 done
 done
 done
