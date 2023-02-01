e92c9979440efcba282e109f35edfe9be57d27fa
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2023-02-01 16:03:15 +0000

    a

--- warnke-skripts/createMinimalListOfFailingTests.sh
+++ warnke-skripts/createMinimalListOfFailingTests.sh
@@ -21,7 +21,7 @@ cat resources/tests/all \
  | grep -w -v -F -f resources/tests/failed  \
  > tmp/blacklist1
 truncate -s0 resources/tests/blacklist
-grep -v -F -f tmp/blacklist1 resources/tests/all > resources/tests/blacklist
+grep -w -v -F -f tmp/blacklist1 resources/tests/all > resources/tests/blacklist
 grep -v " - in simulator - " tmp/blacklist1 > tmp/blacklist2
 if [[ $(wc -l < tmp/blacklist2) -ge 500 ]]
 then
@@ -55,7 +55,7 @@ grep org.junit.runners.model.TestTimedOutException x -B1 \
  | sed "s/.*\[jvm\] > //g" >> resources/tests/timeout
 if [ -s "resources/tests/timeout" ]
 then
-grep -v -F -f resources/tests/timeout x > tmp/x ; mv tmp/x x
+grep -w -v -F -f resources/tests/timeout x > tmp/x ; mv tmp/x x
 fi
 
 grep lupos.*FAILED x \
@@ -64,7 +64,7 @@ grep lupos.*FAILED x \
  | sed "s/.*\[jvm\] > //g" >> resources/tests/failed
 if [ -s "resources/tests/failed" ]
 then
-grep -v -F -f resources/tests/failed x > tmp/x ; mv tmp/x x
+grep -w -v -F -f resources/tests/failed x > tmp/x ; mv tmp/x x
 fi
 
 grep lupos.*PASSED x \
@@ -73,7 +73,7 @@ grep lupos.*PASSED x \
  | sed "s/.*\[jvm\] > //g" >> resources/tests/passed
 if [ -s "resources/tests/passed" ]
 then
-  grep -v -F -f resources/tests/passed x > tmp/x ; mv tmp/x x
+  grep -w -v -F -f resources/tests/passed x > tmp/x ; mv tmp/x x
 fi
 
 grep lupos.*STARTED x \
@@ -92,9 +92,9 @@ cat resources/tests/failed \
  | tr -cd '\11\12\15\40-\176' | sed "s/\[[0-9]*m//g" | sed "s/\[0K//g" \
  | sort| uniq > tmp/x ; mv tmp/x resources/tests/failed
 
-grep -v -F -f resources/tests/failed resources/tests/passed > tmp/x
+grep -w -v -F -f resources/tests/failed resources/tests/passed > tmp/x
 mv tmp/x resources/tests/passed
-grep -v -F -f resources/tests/timeout resources/tests/passed > tmp/x
+grep -w -v -F -f resources/tests/timeout resources/tests/passed > tmp/x
 mv tmp/x resources/tests/passed
 
 done
