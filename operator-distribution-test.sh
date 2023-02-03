8251dece87fe47a39e3f75084fbb2fdb2f6f7766
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2023-02-03 09:44:06 +0000

    a

--- operator-distribution-test.sh
+++ operator-distribution-test.sh
@@ -3,62 +3,49 @@
 rm -rf *.svg *.dot
 cmd=$(./launcher.main.kts --dryMode=Enable --run --mainClass=Launch_Simulator_Config | grep ^exec | sed "s/exec :: //g")
 
+tasksfile="tasks.sh"
+truncate -s0 $tasksfile
 for optimizer in operator-distribution-test-optimizer-topology-assisted.json operator-distribution-test-optimizer-default.json operator-distribution-test-optimizer-topology-only.json
 do
-#for sosa in src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID.json src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID10.json
 for sosa in src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID.json src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID10.json src/luposdate3000_simulator_db/src/jvmMain/resources/ontology/campusSOSAInternalID20.json
 do
 #for topology in src/luposdate3000_simulator_db/src/jvmMain/resources/topology/*16DB.json
 for topology in src/luposdate3000_simulator_db/src/jvmMain/resources/topology/Random16DB.json
 do
-for dataDistribution in src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_key.json src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_id_S_all_collations.json
+for dataDistribution in src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_key.json src/luposdate3000_simulator_db/src/jvmMain/resources/dataDistribution/luposdate3000_by_id_*.json
 do
 for programDistribution in src/luposdate3000_simulator_db/src/jvmMain/resources/programDistribution/distributed*
 do
-for routing in src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_AllShortestPath.json
-#for routing in src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_AllShortestPath.json src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_RPL_Fast.json
+for routing in src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_AllShortestPath.json src/luposdate3000_simulator_db/src/jvmMain/resources/routing/routing_RPL_Fast.json
 do
 for joindistributionlocation in src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_distribution*
 do
-echo $cmd \
-$sosa \
-$topology \
-$programDistribution \
-src/luposdate3000_simulator_db/src/jvmMain/resources/queries/SOSA_QueriesExtended.json \
-$dataDistribution \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000.json \
-$joindistributionlocation \
-src/luposdate3000_simulator_db/src/jvmMain/resources/multicast/luposdate3000MulticastEnabled.json \
-$routing \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_local_execution_enabled.json \
-$optimizer \
-operator-distribution-test.json
-
-$cmd \
-$sosa \
-$topology \
-$programDistribution \
-src/luposdate3000_simulator_db/src/jvmMain/resources/queries/SOSA_QueriesExtended.json \
-$dataDistribution \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000.json \
-$joindistributionlocation \
-src/luposdate3000_simulator_db/src/jvmMain/resources/multicast/luposdate3000MulticastEnabled.json \
-$routing \
-src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_local_execution_enabled.json \
-$optimizer \
-operator-distribution-test.json
-done
-done
-done
-done
-done
-done
-done
-
-exit
-for f in *.dot
+for relocateOperatorsIfTooMuchDataIsSent in src/luposdate3000_simulator_db/src/jvmMain/resources/relocateOperatorsIfTooMuchDataIsSent/*
 do
-   echo fdp $f -Tsvg -O${f%.dot}.svg
-   fdp $f -Tsvg -O${f%.dot}.svg
+echo "echo" $cmd \
+$sosa \
+$topology \
+$programDistribution \
+src/luposdate3000_simulator_db/src/jvmMain/resources/queries/SOSA_QueriesExtended.json \
+$dataDistribution \
+src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000.json \
+$joindistributionlocation \
+src/luposdate3000_simulator_db/src/jvmMain/resources/multicast/luposdate3000MulticastEnabled.json \
+$routing \
+src/luposdate3000_simulator_db/src/jvmMain/resources/luposdate3000_local_execution_enabled.json \
+$optimizer \
+$relocateOperatorsIfTooMuchDataIsSent \
+operator-distribution-test.json >> $tasksfile
+
+done
+done
+done
+done
+done
+done
+done
 done
 
+cat $tasksfile | sed "s/java -Xmx100g/java -Xmx10g/g" | shuf > ${tasksfile}.tmp
+mv ${tasksfile}.tmp $tasksfile
+cat ${tasksfile} | parallel -j 20
