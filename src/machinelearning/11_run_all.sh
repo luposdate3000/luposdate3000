f12a349b9ca6b20ea39260fece733729314bfb33
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2023-04-11 10:00:46 +0000

    a

--- src/machinelearning/11_run_all.sh
+++ src/machinelearning/11_run_all.sh
@@ -4,7 +4,7 @@ rm -rf tasks
 mkdir tasks _tmpdata/model/
 i=0
 #for x in 3_3 4_4 5_5 6_6 7_7 8_8 9_9 10_10 11_11 12_12 13_13 14_14 15_15 16_16 17_17 18_18 19_19 20_20 3_20 5_18 7_16 10_20 12_18 12_20
-for x in 3_3 4_4 5_5 3_4 3_5 4_5
+for x in 3_3 4_4 5_5 3_4 3_5 4_5 3_6 4_6 5_6 6_6
 do
 arr=(${x//_/ })
 ii=$(printf "%04d" $i)
@@ -19,7 +19,7 @@ for y in 2048 4096 8192 16384 32768 65536 131072 262144 393216 524288 655360 786
 # 1179648 1310720 1441792 1572864
 do
 ii=$(printf "%04d" $i)
-echo "./12_joinopti_agent_eval.py 1 5 20 0.7 /src/luposdate3000/src/machinelearning/_tmpdata/complete.n3.nt  _tmpdata/model/model_${arr[0]}_${arr[1]}_20_0.7_${y}.model" > tasks/$ii.sh
+echo "./12_joinopti_agent_eval.py 1 6 20 0.7 /src/luposdate3000/src/machinelearning/_tmpdata/complete.n3.nt  _tmpdata/model/model_${arr[0]}_${arr[1]}_20_0.7_${y}.model" > tasks/$ii.sh
 echo "retVal=\$?" >> tasks/$ii.sh
 echo "if [ \$retVal -eq 0 ]; then" >> tasks/$ii.sh
 echo "truncate -s0 tasks/$ii.sh" >> tasks/$ii.sh
