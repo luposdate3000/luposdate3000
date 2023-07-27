3ca022e7cd04846aead4f84edd0f3db4f084eccc
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2023-07-27 09:11:45 +0000

    a

--- src/luposdate3000_spa_client/src/endpointLuposdate3000Browser.js
+++ src/luposdate3000_spa_client/src/endpointLuposdate3000Browser.js
@@ -1,6 +1,16 @@
+const luposdate3000 = require("../../luposdate3000_endpoint/build/distributions/luposdate3000_endpoint.js")
+
 function evaluate(env, sparql, rdf, useRDF, callback) {
+    if (useRDF) {
+        luposdate3000.lupos.endpoint.LuposdateEndpoint.close()
+        env.instance = luposdate3000.lupos.endpoint.LuposdateEndpoint.initialize()
+        luposdate3000.lupos.endpoint.LuposdateEndpoint.import_turtle_string(env.instance, rdf);
+    }
+    var response = luposdate3000.lupos.endpoint.LuposdateEndpoint.evaluate_sparql_to_result_b(env.instance, sparql);
+    console.log(response)
 }
 export function init(data) {
     data.evaluate = evaluate;
+    data.instance = luposdate3000.lupos.endpoint.LuposdateEndpoint.initialize()
     return data
 }
