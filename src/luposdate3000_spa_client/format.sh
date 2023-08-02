f98c27740867396a075a3b5add2c509ad459ce86
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2023-08-02 08:44:23 +0000

    format

--- src/luposdate3000_spa_client/format.sh
+++ src/luposdate3000_spa_client/format.sh
@@ -1,2 +1,7 @@
-js-beautify *html *js src/*js *json resources/**/*json resources/*json
+js-beautify *js src/*js
 tidy -config .tidy.config src/index.html
+for f in $(find resources -name "*json") *json
+do
+content=$(cat $f)
+jq . -S <<<$content >$f
+done
