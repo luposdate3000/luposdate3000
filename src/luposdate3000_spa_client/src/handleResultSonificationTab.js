faaccdb29f237398c0a62428553b75b49eee3b2c
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2023-08-02 09:17:26 +0000

    a

--- src/luposdate3000_spa_client/src/handleResultSonificationTab.js
+++ src/luposdate3000_spa_client/src/handleResultSonificationTab.js
@@ -15,14 +15,26 @@ import {
 
 var network = null;
 var cacheGraph = []
+var cacheAnimation = []
+var animationStep = 0
 export function updateResultSonificationTab(result) {
-console.log(result)
-    if ("optimization_steps" in result) {
+    if ("optimization_steps" in result && "animation" in result) {
+        cacheAnimation = []
+        animationStep = 0
         document.querySelector("#result-sonification-tab-nav-item").style.display = "list-item"
         cacheGraph = result.optimization_steps[result.optimization_steps.length - 1]
         for (const n of cacheGraph.nodes) {
             n.color = getColorByType(n.label.split(' ')[0])
         }
+        for (const n of result.animation) {
+            cacheAnimation.push({
+                "from": n[0],
+                "to": n[1],
+                "label": n[2],
+                "id": n[3]
+            })
+        }
+        console.log(cacheAnimation)
         showSonification()
     } else {
         document.querySelector("#result-sonification-tab-nav-item").style.display = "none"
@@ -44,4 +56,51 @@ function showSonification() {
 
 jquery("#result-sonification-tab-trigger").on("click", function() {
     network.fit()
+});
+
+var animationSpeed = 1
+var animationState = 'stop';
+jquery("#result-sonification-btn-fbw").on("click", function() {
+    animationStep = 0
+});
+
+jquery("#result-sonification-btn-bw").on("click", function() {
+    if (animationSpeed >= 0) {
+        animationSpeed = -1
+    }
+});
+
+jquery("#result-sonification-btn-fw").on("click", function() {
+    if (animationSpeed >= 0) {
+        animationSpeed = 1
+    }
+});
+
+jquery("#result-sonification-btn-ffw").on("click", function() {
+    animationStep = cacheAnimation.length - 1
+});
+
+jquery("#result-sonification-btn-play").on("click", function() {
+    if (animationState == 'stop') {
+        animationState = 'play';
+        var button = d3.select("#button_play").classed('btn-success', true);
+        button.select("i").attr('class', "fa fa-pause");
+animationSpeed = 1
+    } else if (animationState == 'play' || animationState == 'resume') {
+        animationState = 'pause';
+        d3.select("#button_play i").attr('class', "fa fa-play");
+animationSpeed = 0
+    } else if (animationState == 'pause') {
+        animationState = 'resume';
+        d3.select("#button_play i").attr('class', "fa fa-pause");
+animationSpeed = 1
+    }
+    console.log("button play pressed, play was " + animationState);
+});
+
+jquery("#result-sonification-btn-stop").on("click", function() {
+    animationState = 'stop';
+    var button = d3.select("#button_play").classed('btn-success', false);
+    button.select("i").attr('class', "fa fa-play");
+    console.log("button stop invoked.");
 });
