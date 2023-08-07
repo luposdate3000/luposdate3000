c740327de0a14a4d75d177951310c89700d730a3
Benjamin Warnke <benjamin.warnke@uni-luebeck.de>
2023-08-07 06:12:17 +0000

    a

--- warnke-skripts/createMinimalListOfFailingTests.py
+++ warnke-skripts/createMinimalListOfFailingTests.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env -S python3 -OO -u
 import os
-
-os.makedirs("resources/tests")
+try:
+ os.makedirs("resources/tests")
+except:
+ pass
 srcCode=[]
 with open ("./src/luposdate3000_test/src/commonMain/kotlin/lupos/test/SparqlTestSuiteConverterToUnitTest.kt") as f:
  for l in f:
-  idx=l.index("minifyMode")
-   if idx<0:
-    srcCode.append(l.replace("\n",""))
+   if "minifyMode: Boolean" in l:
+    srcCode.append(l[0:l.index("minifyMode")]+"minifyMode: Boolean = true")
    else:
-    srcCode.append(l[0:idx]+": Boolean = true")
+    srcCode.append(l.replace("\n",""))
 with open ("./src/luposdate3000_test/src/commonMain/kotlin/lupos/test/SparqlTestSuiteConverterToUnitTest.kt", "w") as f:
  for l in srcCode:
   f.write(l+"\n")
